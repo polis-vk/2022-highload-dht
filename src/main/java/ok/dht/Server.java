d6aa85cd9ff8f4d17e408af84d3968edcec48abc
Vadim Tsesko <incubos@users.noreply.github.com>
2022-09-14 13:53:49 +0300

    Add Server and test stubs with refreshed dependencies

---
+++ src/main/java/ok/dht/Server.java
@@ -0,0 +1,17 @@
+package ok.dht;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Basic server stub.
+ *
+ * @author incubos
+ */
+public class Server {
+    private static final Logger log = LoggerFactory.getLogger(Server.class);
+
+    public static void main(String[] args) {
+        log.error("Implement me!");
+    }
+}
