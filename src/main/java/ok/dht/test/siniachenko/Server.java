c895f991f07f952ed176d825672e761a8b980f24
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-09-25 20:39:19 +0300

    added "Hello" answering Server and Service

---
+++ src/main/java/ok/dht/test/siniachenko/Server.java
@@ -0,0 +1,29 @@
+package ok.dht.test.siniachenko;
+
+import ok.dht.ServiceConfig;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        int port = 12345;
+        String url = String.format("http://localhost:%d", port);
+        ServiceConfig config;
+        try {
+            config = new ServiceConfig(
+                    port,
+                    url,
+                    Collections.singletonList(url),
+                    Files.createTempDirectory("server")
+            );
+        } catch (IOException e) {
+            System.err.println("Cannot create server directory \"server\"");
+            throw e;
+        }
+        new Service(config).start().get(1, TimeUnit.SECONDS);
+        System.out.printf("Started Server on %s%n", url);
+    }
+}
