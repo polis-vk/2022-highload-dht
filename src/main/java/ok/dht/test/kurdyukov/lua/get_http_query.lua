602ef99184af8336dcc2eef1cb69cd1d9098514b
KirillKurdyukov <kurdyukov_kirya@mail.ru>
2022-09-27 01:00:36 +0300

    added new report

--- src/main/java/ok/dht/test/kurdyukov/lua/get_http_query.lua
+++ src/main/java/ok/dht/test/kurdyukov/lua/get_http_query.lua
@@ -1,7 +1,7 @@
 cnt = 0
 
 request = function()
-    path = "/v0/entity?id=" .. cnt
+    path = "/v0/entity?id=" .. math.random(1, 10000)
     wrk.method = "GET"
     cnt = cnt + 1
     return wrk.format(nil, path)
