9ac13141790698a274d1549498b9582a1e390f6d
KirillKurdyukov <kurdyukov_kirya@mail.ru>
2022-09-27 01:00:36 +0300

    added new report

---
+++ src/main/java/ok/dht/test/kurdyukov/Main.java
@@ -0,0 +1,32 @@
+package ok.dht.test.kurdyukov;
+
+import ok.dht.Service;
+import ok.dht.ServiceConfig;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+    private Main() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Service service = new ServiceImpl.Factory().create(
+                new ServiceConfig(
+                        4242,
+                        "http://localhost",
+                        List.of("http://localhost"),
+                        Files.createTempDirectory("service")
+                )
+        );
+
+        try {
+            service.start().get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
