ab4ba1f63ab60356eaa1f8f5153aa0172d2df5d6
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-09-28 00:09:13 +0300

    added requests handlers to Service

--- src/main/java/ok/dht/test/siniachenko/Service.java
+++ src/main/java/ok/dht/test/siniachenko/Service.java
@@ -2,16 +2,32 @@ package ok.dht.test.siniachenko;
 
 import ok.dht.ServiceConfig;
 import ok.dht.test.ServiceFactory;
-import one.nio.http.HttpServer;
-import one.nio.http.HttpServerConfig;
+import one.nio.http.*;
 import one.nio.server.AcceptorConfig;
 
 import java.io.IOException;
-import java.lang.annotation.Annotation;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class Service implements ok.dht.Service {
 
+    public static final Response NOT_FOUND_RESPONSE = new Response(
+            Response.NOT_FOUND,
+            Response.EMPTY
+    );
+    public static final Response CREATED_RESPONSE = new Response(
+            Response.CREATED,
+            Response.EMPTY
+    );
+    public static final Response ACCEPTED_RESPONSE = new Response(
+            Response.ACCEPTED,
+            Response.EMPTY
+    );
+    public static final Response BAD_REQUEST_RESPONSE = new Response(
+            Response.BAD_REQUEST,
+            Response.EMPTY
+    );
     private final ServiceConfig config;
     private HttpServer server;
 
@@ -23,6 +39,7 @@ public class Service implements ok.dht.Service {
     public CompletableFuture<?> start() throws IOException {
         server = new HttpServer(createConfigFromPort(config.selfPort()));
         server.start();
+        System.out.println("Service started on " + config.selfUrl());
         server.addRequestHandlers(this);
         return CompletableFuture.completedFuture(null);
     }
@@ -40,6 +57,53 @@ public class Service implements ok.dht.Service {
     public CompletableFuture<?> stop() throws IOException {
         server.stop();
         return CompletableFuture.completedFuture(null);
+    }
+
+    @Path("/v0/entity")
+    @RequestMethod(Request.METHOD_GET)
+    public Response getEntity(@Param(value = "id", required = true) String id) {
+        if (id.isEmpty()) {
+            return BAD_REQUEST_RESPONSE;
+        }
+        byte[] stored = db.get(id);
+        if (stored == null) {
+            return NOT_FOUND_RESPONSE;
+        } else {
+            return new Response(
+                    Response.OK,
+                    stored
+            );
+        }
+    }
+
+    @Path("/v0/entity")
+    @RequestMethod(Request.METHOD_PUT)
+    public Response upsertEntity(
+            @Param(value = "id", required = true) String id,
+            Request request
+    ) {
+        if (id.isEmpty()) {
+            return BAD_REQUEST_RESPONSE;
+        }
+        db.put(id, request.getBody());
+        return CREATED_RESPONSE;
+    }
+
+    @Path("/v0/entity")
+    @RequestMethod(Request.METHOD_DELETE)
+    public Response deleteEntity(
+            @Param(value = "id", required = true) String id
+    ) {
+        if (id.isEmpty()) {
+            return BAD_REQUEST_RESPONSE;
+        }
+        db.remove(id);
+        return ACCEPTED_RESPONSE;
+    }
+
+    @RequestMethod(Request.METHOD_GET)
+    public Response defaultHandleRequest() {
+        return BAD_REQUEST_RESPONSE;
     }
 
     @ServiceFactory(stage = 1, week = 1)
