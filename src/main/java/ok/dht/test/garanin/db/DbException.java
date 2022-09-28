07149821cdd236ada101b64c3164e828ab746f1a
Garanin Artem <fellowweeb@yandex-team.ru>
2022-09-28 23:13:08 +0300

    stage 1: garanin

---
+++ src/main/java/ok/dht/test/garanin/db/DbException.java
@@ -0,0 +1,20 @@
+package ok.dht.test.garanin.db;
+
+import java.io.IOException;
+
+public class DbException extends IOException {
+    public DbException() {
+    }
+
+    public DbException(String message) {
+        super(message);
+    }
+
+    public DbException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DbException(Throwable cause) {
+        super(cause);
+    }
+}
