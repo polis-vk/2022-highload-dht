cb463fcb19ca811c22c2716e9e87779023749676
Garanin Artem <fellowweeb@yandex-team.ru>
2022-09-28 23:27:45 +0300

    stage 1: fix codeclimat

--- src/main/java/ok/dht/test/garanin/DhtServer.java
+++ src/main/java/ok/dht/test/garanin/DhtServer.java
@@ -1,12 +1,17 @@
 package ok.dht.test.garanin;
 
 import ok.dht.ServiceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public final class DhtServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(DhtServer.class);
+
     private DhtServer() {
         // Only main method
     }
@@ -23,7 +28,7 @@ public final class DhtServer {
                     dir
             );
             new DhtService(cfg).start().get(1, TimeUnit.SECONDS);
-            System.err.println("Socket is ready: " + url);
+            logger.info("Socket is ready: " + url);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
