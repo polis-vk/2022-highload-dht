3d3745d929e71c6a08023fe234e54a8c740d66f7
Garanin Artem <fellowweeb@yandex-team.ru>
2022-09-28 23:22:13 +0300

    stage 1: fix codeclimat

--- src/main/java/ok/dht/test/garanin/DhtServer.java
+++ src/main/java/ok/dht/test/garanin/DhtServer.java
@@ -6,23 +6,26 @@ import java.nio.file.Path;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
-public class DhtServer {
+public final class DhtServer {
     private DhtServer() {
         // Only main method
     }
 
-    public static void main(String[] args) throws Exception {
-        int port = 19234;
-        String url = "http://localhost:" + port;
-        var dir = Path.of(".");
-        ServiceConfig cfg = new ServiceConfig(
-                port,
-                url,
-                Collections.singletonList(url),
-                dir
-        );
-        new DhtService(cfg).start().get(1, TimeUnit.SECONDS);
-        System.out.println("Socket is ready: " + url);
-        System.out.println(dir.toAbsolutePath().normalize());
+    public static void main(String[] args) {
+        try {
+            int port = 19234;
+            String url = "http://localhost:" + port;
+            var dir = Path.of(".");
+            ServiceConfig cfg = new ServiceConfig(
+                    port,
+                    url,
+                    Collections.singletonList(url),
+                    dir
+            );
+            new DhtService(cfg).start().get(1, TimeUnit.SECONDS);
+            System.err.println("Socket is ready: " + url);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
