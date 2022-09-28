4e56161ae6b154eddcc4b682518c5ee5ad12991e
Brat-vseznamus <fvixnin@gmail.com>
2022-09-28 23:56:52 +0300

    add report + data

--- src/main/java/ok/dht/test/vihnin/resources/put.lua
+++ src/main/java/ok/dht/test/vihnin/resources/put.lua
@@ -1,6 +1,13 @@
 math.randomseed(os.time())
        
 
+
+
 function request()
-    return wrk.format("PUT", "/v0/entity?id=" .. math.random(1, 1024 * 1024), {}, "" .. math.random(1, 1024 * 1024))
+    return wrk.format(
+        "PUT", 
+        "/v0/entity?id=" .. math.random(1, 1024 * 1024), 
+        {}, 
+        string.rep("data", 100)
+    )
 end
