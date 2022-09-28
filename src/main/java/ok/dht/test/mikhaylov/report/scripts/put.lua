1367145d81ed0e168103c9d2bf0d7e70fcd99b88
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2022-09-28 22:47:59 +0300

    report done

--- src/main/java/ok/dht/test/mikhaylov/report/scripts/put.lua
+++ src/main/java/ok/dht/test/mikhaylov/report/scripts/put.lua
@@ -2,7 +2,7 @@ index = 0
 request = function()
     path = "/v0/entity?id=" .. index
     wrk.method = "PUT"
-    wrk.body = string.rep("value#" .. index, 100)
+    wrk.body = "value#" .. index
     index = index + 1
     return wrk.format(nil, path)
 end
