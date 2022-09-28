c9402f18cc292df900504fa7de90399bc5733bdd
Kirill Pobedonostsev <kirill.pobedonostsev@vk.team>
2022-09-28 15:59:02 +0300

    Fix specified issues gst

--- src/main/java/ok/dht/test/pobedonostsev/CustomHttpServer.java
+++ src/main/java/ok/dht/test/pobedonostsev/CustomHttpServer.java
@@ -5,10 +5,16 @@ import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
 import one.nio.http.Request;
 import one.nio.http.Response;
+import one.nio.net.Session;
+import one.nio.server.SelectorThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class CustomHttpServer extends HttpServer {
+    private static final Logger LOG = LoggerFactory.getLogger(CustomHttpServer.class);
+
     public CustomHttpServer(HttpServerConfig config, Object... routers) throws IOException {
         super(config, routers);
     }
@@ -19,13 +25,23 @@ public class CustomHttpServer extends HttpServer {
             super.handleRequest(request, session);
         } catch (IOException e) {
             session.sendResponse(new Response(Response.INTERNAL_ERROR, Response.EMPTY));
+            LOG.error("Unexpected error", e);
         }
-        session.scheduleClose();
     }
 
     @Override
     public void handleDefault(Request request, HttpSession session) throws IOException {
         Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
         session.sendResponse(response);
+    }
+
+    @Override
+    public synchronized void stop() {
+        for (SelectorThread selector : selectors) {
+            for (Session session : selector.selector) {
+                session.close();
+            }
+        }
+        super.stop();
     }
 }
