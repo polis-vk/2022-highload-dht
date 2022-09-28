8cccb3b88713c393dfedfbf0d31e9711c7abe3c4
ayasevich <artem.yasevich@mail.ru>
2022-09-28 17:38:10 +0300

    Adds report

--- src/main/java/ok/dht/test/yasevich/profiling/scripts/get.lua
+++ src/main/java/ok/dht/test/yasevich/profiling/scripts/get.lua
@@ -1,5 +1,5 @@
 request = function()
-    local id = math.random(0,50000000)
+    local id = math.random(0,25000000)
     path = "/v0/entity?id=key" .. id
     wrk.method = "GET"
     return wrk.format(nil, path, headers)
