776c05b2ed029fa04c362c26bcad4676bccaca5d
KirillKurdyukov <kurdyukov_kirya@mail.ru>
2022-09-28 19:11:21 +0300

    update DB on levelDB + update report + heatmap

--- src/main/java/ok/dht/test/kurdyukov/db/dao/storage/Data.java
+++
@@ -1,8 +0,0 @@
-package ok.dht.test.kurdyukov.db.dao.storage;
-
-import jdk.incubator.foreign.MemorySegment;
-import ok.dht.test.kurdyukov.db.base.Entry;
-
-public interface Data extends Iterable<Entry<MemorySegment>> {
-
-}
