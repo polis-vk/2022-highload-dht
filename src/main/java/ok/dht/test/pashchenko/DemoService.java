d11fad9cfa0aa0be2409758cb6c8135e829ce7b4
Aleksandr <al.pashchenko8@gmail.com>
2022-09-29 21:49:34 +0300

    .

--- src/main/java/ok/dht/test/pashchenko/DemoService.java
+++ src/main/java/ok/dht/test/pashchenko/DemoService.java
@@ -1,11 +1,16 @@
 package ok.dht.test.pashchenko;
 
+import jdk.incubator.foreign.MemorySegment;
 import ok.dht.Service;
 import ok.dht.ServiceConfig;
 import ok.dht.test.ServiceFactory;
+import ok.dht.test.pashchenko.dao.Config;
+import ok.dht.test.pashchenko.dao.Entry;
 import ok.dht.test.pashchenko.dao.MemorySegmentDao;
 import one.nio.http.*;
+import one.nio.net.Session;
 import one.nio.server.AcceptorConfig;
+import one.nio.server.SelectorThread;
 import one.nio.util.Utf8;
 
 import java.io.IOException;
@@ -23,25 +28,71 @@ public class DemoService implements Service {
 
     @Override
     public CompletableFuture<?> start() throws IOException {
-        server = new HttpServer(createConfigFromPort(config.selfPort()));
-        server.start();
+        dao = new MemorySegmentDao(new Config(config.workingDir(), 1048576L));
+        server = new HttpServer(createConfigFromPort(config.selfPort())) {
+            @Override
+            public void handleDefault(Request request, HttpSession session) throws IOException {
+                Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
+                session.sendResponse(response);
+            }
+
+            @Override
+            public synchronized void stop() {
+                super.stop();
+                for (SelectorThread selector : selectors) {
+                    for (Session session : selector.selector) {
+                        session.close();
+                    }
+                }
+            }
+        };
         server.addRequestHandlers(this);
+        server.start();
+
         return CompletableFuture.completedFuture(null);
     }
 
     @Override
     public CompletableFuture<?> stop() throws IOException {
+        server.stop();
+        dao.close();
         return CompletableFuture.completedFuture(null);
     }
 
-    @Path("/")
+    @Path("/v0/entity")
     @RequestMethod(Request.METHOD_GET)
-    public Response handleGet() {
-        return new Response(
-                Response.OK,
-                Utf8.toBytes("HELLO\n")
-        );
+    public Response handleGet(@Param(value = "id") String id) {
+        if (id == null || id.isEmpty()) {
+            return new Response(Response.BAD_REQUEST, Response.EMPTY);
+        }
+        Entry entry = dao.get(MemorySegment.ofArray(Utf8.toBytes(id)));
+        if (entry == null) {
+            return new Response(Response.NOT_FOUND, Response.EMPTY);
+        }
+        return new Response(Response.OK, entry.value().toByteArray());
     }
+
+    @Path("/v0/entity")
+    @RequestMethod(Request.METHOD_PUT)
+    public Response handlePut(Request request, @Param(value = "id") String id) {
+        if (id == null || id.isEmpty()) {
+            return new Response(Response.BAD_REQUEST, Response.EMPTY);
+        }
+        dao.upsert(new Entry(MemorySegment.ofArray(Utf8.toBytes(id)), MemorySegment.ofArray(request.getBody())));
+        return new Response(Response.CREATED, Response.EMPTY);
+    }
+
+
+    @Path("/v0/entity")
+    @RequestMethod(Request.METHOD_DELETE)
+    public Response handleDelete(@Param(value = "id", required = true) String id) {
+        if (id == null || id.isEmpty()) {
+            return new Response(Response.BAD_REQUEST, Response.EMPTY);
+        }
+        dao.upsert(new Entry(MemorySegment.ofArray(Utf8.toBytes(id)), null));
+        return new Response(Response.ACCEPTED, Response.EMPTY);
+    }
+
 
     private static HttpServerConfig createConfigFromPort(int port) {
         HttpServerConfig httpConfig = new HttpServerConfig();
@@ -52,7 +103,7 @@ public class DemoService implements Service {
         return httpConfig;
     }
 
-    @ServiceFactory(stage = 0, week = 1)
+    @ServiceFactory(stage = 1, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
     public static class Factory implements ServiceFactory.Factory {
 
         @Override
