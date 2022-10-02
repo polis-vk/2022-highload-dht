f1caecdbc83294d37c4d3ddcbc35ff4b5b054515
Daniil Ushkov <udav318@yandex-team.ru>
2022-10-02 22:37:57 +0300

    stage 2: init

--- src/main/java/ok/dht/test/ushkov/RocksDBHttpServer.java
+++ src/main/java/ok/dht/test/ushkov/RocksDBHttpServer.java
@@ -6,56 +6,39 @@ import one.nio.http.Request;
 import one.nio.http.Response;
 import one.nio.net.Session;
 import one.nio.server.SelectorThread;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class RocksDBHttpServer extends HttpServer {
-    private final ExecutorService executor;
-    private static final Logger LOG = LoggerFactory.getLogger(RocksDBHttpServer.class);
+    private ExecutorService executor;
 
     public RocksDBHttpServer(RocksDBHttpServerConfig config, Object... routers) throws IOException {
         super(config, routers);
-        int processors = Runtime.getRuntime().availableProcessors();
-        int workers = config.workers >= 0 ? config.workers : processors;
         executor = new ThreadPoolExecutor(
-                workers,
-                workers,
+                config.workers,
+                config.workers,
                 0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>(config.queueCapacity)
+                new ArrayBlockingQueue<>(config.queueCapacity)
         );
     }
 
     @Override
     public void handleRequest(Request request, HttpSession session) throws IOException {
-        try {
-            executor.execute(() -> doHandleRequest(request, session));
-        } catch (RejectedExecutionException e) {
-            LOG.info("drop request, queue is full");
-            session.sendError(Response.SERVICE_UNAVAILABLE, e.getMessage());
-        }
-    }
-
-    private void doHandleRequest(Request request, HttpSession session) {
-        try {
-            super.handleRequest(request, session);
-        } catch (Exception e) {
-            handleException(session, e);
-        }
-    }
-
-    private void handleException(HttpSession session, Exception e) {
-        try {
-            session.sendError(Response.BAD_REQUEST, e.getMessage());
-        } catch (IOException e1) {
-            LOG.error("could not send response", e1);
-        }
+        executor.execute(() -> {
+            try {
+                super.handleRequest(request, session);
+            } catch (Exception e) {
+                try {
+                    session.sendError(Response.BAD_REQUEST, e.getMessage());
+                } catch (IOException e1) {
+                    // Do nothing
+                }
+            }
+        });
     }
 
     @Override
@@ -72,11 +55,7 @@ public class RocksDBHttpServer extends HttpServer {
                 session.socket().close();
             }
         }
-        executor.shutdown();
-        super.stop();
-    }
 
-    public boolean awaitStop(long timeout, TimeUnit unit) throws InterruptedException {
-        return executor.awaitTermination(timeout, unit);
+        super.stop();
     }
 }
