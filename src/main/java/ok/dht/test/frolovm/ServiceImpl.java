dffb8130aa548a5f463adc278b332f76b8db9155
mk17ru <mk17.ru@mail.ru>
2022-10-02 23:31:15 +0300

    add solution

--- src/main/java/ok/dht/test/frolovm/ServiceImpl.java
+++ src/main/java/ok/dht/test/frolovm/ServiceImpl.java
@@ -12,8 +12,6 @@ import ok.dht.test.frolovm.artyomdrozdov.MemorySegmentDao;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
-import one.nio.http.Param;
-import one.nio.http.Path;
 import one.nio.http.Request;
 import one.nio.http.Response;
 import one.nio.net.Session;
@@ -21,15 +19,31 @@ import one.nio.server.AcceptorConfig;
 import one.nio.server.SelectorThread;
 import one.nio.util.Utf8;
 import java.io.IOException;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ServiceImpl implements Service {
 
     public static final int FLUSH_THRESHOLD_BYTES = 1_048_576;
+    public static final String PATH_ENTITY = "/v0/entity";
+    public static final String PARAM_ID_NAME = "id=";
     private static final byte[] BAD_ID = Utf8.toBytes("Given id is bad.");
-
     private static final byte[] NO_SUCH_METHOD = Utf8.toBytes("No such method.");
+    private static final int CORE_POLL_SIZE = 8;
+    private static final int MAXIMUM_POLL_SIZE = 64;
+    private static final int KEEP_ALIVE_TIME = 5;
+    private static final int QUEUE_CAPACITY = 128;
     private final ServiceConfig config;
+    private final ExecutorService executorService = new ThreadPoolExecutor(
+            CORE_POLL_SIZE,
+            MAXIMUM_POLL_SIZE,
+            KEEP_ALIVE_TIME,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(QUEUE_CAPACITY)
+    );
     private Dao<MemorySegment, Entry<MemorySegment>> dao;
     private HttpServer server;
 
@@ -78,6 +92,25 @@ public class ServiceImpl implements Service {
             createDao();
         }
         server = new HttpServer(createConfigFromPort(config.selfPort())) {
+
+            @Override
+            public void handleRequest(Request request, HttpSession session) {
+                executorService.submit(
+                        () -> {
+                            try {
+                                if (request.getPath().equals(PATH_ENTITY)) {
+                                    Response response = entityHandler(request.getParameter(PARAM_ID_NAME), request);
+                                    session.sendResponse(response);
+                                } else {
+                                    handleDefault(request, session);
+                                }
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+            }
+
             @Override
             public void handleDefault(Request request, HttpSession session) throws IOException {
                 session.sendResponse(emptyResponse(Response.BAD_REQUEST));
@@ -109,8 +142,7 @@ public class ServiceImpl implements Service {
         }
     }
 
-    @Path("/v0/entity")
-    public Response entityHandler(@Param(value = "id", required = true) String id, Request request) {
+    private Response entityHandler(String id, Request request) {
         if (!checkId(id)) {
             return new Response(Response.BAD_REQUEST, BAD_ID);
         }
