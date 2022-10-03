c3b30ef6680aed3aaa2e1325a50ea407b0bac644
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2022-10-03 20:01:42 +0300

    Future fixes and take elements from deque fix

--- src/main/java/ok/dht/test/kiselyov/util/CustomLinkedBlockingDeque.java
+++ src/main/java/ok/dht/test/kiselyov/util/CustomLinkedBlockingDeque.java
@@ -1,9 +1,24 @@
 package ok.dht.test.kiselyov.util;
 
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CustomLinkedBlockingDeque<E> extends LinkedBlockingDeque<E> {
+
+    private final AtomicBoolean isHead = new AtomicBoolean(true);
+
     public CustomLinkedBlockingDeque(int dequeCapacity) {
         super(dequeCapacity);
+    }
+
+    @Override
+    public E take() throws InterruptedException {
+        if (isHead.get()) {
+            isHead.set(!isHead.get());
+            return super.takeFirst();
+        } else {
+            isHead.set(!isHead.get());
+            return super.takeLast();
+        }
     }
 }
