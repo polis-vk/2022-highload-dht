18eff45f9a0a1f36f489bf60d0aa673c234ad4be
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2022-10-03 19:23:39 +0300

    initial commit

---
+++ src/main/java/ok/dht/test/kiselyov/util/CustomLinkedBlockingDeque.java
@@ -0,0 +1,9 @@
+package ok.dht.test.kiselyov.util;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class CustomLinkedBlockingDeque<E> extends LinkedBlockingDeque<E> {
+    public CustomLinkedBlockingDeque(int dequeCapacity) {
+        super(dequeCapacity);
+    }
+}
