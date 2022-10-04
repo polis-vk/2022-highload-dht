7fafa5173625e38a6320d91ee0c6560ef18b176c
ayasevich <artem.yasevich@mail.ru>
2022-10-04 15:33:27 +0300

    Make scripts Mac M1 wrk2 compatible

--- src/main/java/ok/dht/test/yasevich/profiling/scripts/put.lua
+++ src/main/java/ok/dht/test/yasevich/profiling/scripts/put.lua
@@ -1,7 +1,8 @@
 request = function()
-    local id = math.random(0,25000000)
+    local id = math.random(0, 25000000)
+    local value = math.random(0, 25000000)
     path = "/v0/entity?id=key" .. id
     wrk.method = "PUT"
-    wrk.body = "VALUE" .. id
-    return wrk.format(nil, path, headers)
+    wrk.body = "value" .. value
+    return "PUT " .. path .. " HTTP/1.1\r\n" .. "Host: http://localhost:19234\r\nContent-Length: " .. string.len(tostring(value)) .. "\r\n\r\n" .. tostring(value)
 end
