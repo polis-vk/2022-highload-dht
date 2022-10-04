77328ab7039c71a7ed48b233941f54b5aab65221
Mizzza54 <gerasimov.misha2001@gmail.com>
2022-10-04 20:00:28 +0300

    Mass replace

--- src/main/java/ok/dht/test/gerasimov/reports/put.lua
+++
@@ -1,7 +0,0 @@
-raw_path = "/v0/entity?id="
-count = -1
-request = function()
-    count = count + 1
-    path = raw_path .. count
-    return wrk.format("PUT", path, {"Content-Type: text/plain"}, string.rep("highload is a best subject!", 322))
-end
