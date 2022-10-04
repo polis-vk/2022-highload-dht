5524872d22f11d934d4ef7f0c2d337cf8e57cdc8
Veronika <58723287+interestIngc@users.noreply.github.com>
2022-10-04 20:42:37 +0200

    fixes

---
+++ src/main/java/ok/dht/test/drozdov/DemoService.java
@@ -0,0 +1,66 @@
+package ok.dht.test.drozdov;
+
+import ok.dht.Service;
+import ok.dht.ServiceConfig;
+import ok.dht.test.ServiceFactory;
+import one.nio.http.HttpServer;
+import one.nio.http.HttpServerConfig;
+import one.nio.http.Path;
+import one.nio.http.Request;
+import one.nio.http.RequestMethod;
+import one.nio.http.Response;
+import one.nio.server.AcceptorConfig;
+import one.nio.util.Utf8;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+public class DemoService implements Service {
+
+    private final ServiceConfig config;
+    private HttpServer server;
+
+    public DemoService(ServiceConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public CompletableFuture<?> start() throws IOException {
+        server = new HttpServer(createConfigFromPort(config.selfPort()));
+        server.start();
+        server.addRequestHandlers(this);
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public CompletableFuture<?> stop() throws IOException {
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Path("/")
+    @RequestMethod(Request.METHOD_GET)
+    public Response handleGet() {
+        return new Response(
+                Response.OK,
+                Utf8.toBytes("HELLO\n")
+        );
+    }
+
+    private static HttpServerConfig createConfigFromPort(int port) {
+        HttpServerConfig httpConfig = new HttpServerConfig();
+        AcceptorConfig acceptor = new AcceptorConfig();
+        acceptor.port = port;
+        acceptor.reusePort = true;
+        httpConfig.acceptors = new AcceptorConfig[]{acceptor};
+        return httpConfig;
+    }
+
+    @ServiceFactory(stage = 0, week = 1)
+    public static class Factory implements ServiceFactory.Factory {
+
+        @Override
+        public Service create(ServiceConfig config) {
+            return new DemoService(config);
+        }
+    }
+}
