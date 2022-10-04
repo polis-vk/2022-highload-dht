72b9f70cf16e8a2614d8cf368247eac7323861f5
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-10-04 03:50:09 +0300

    deleted reference DB files

--- src/main/java/ok/dht/test/siniachenko/storage/StorageClosedException.java
+++
@@ -1,9 +0,0 @@
-package ok.dht.test.siniachenko.storage;
-
-public class StorageClosedException extends RuntimeException {
-
-    public StorageClosedException(Throwable causedBy) {
-        super(causedBy);
-    }
-
-}
