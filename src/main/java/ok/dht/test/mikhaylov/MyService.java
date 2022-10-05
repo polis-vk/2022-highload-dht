f4238d2ca88c0df94fb3ae9f13c1478e92e313fb
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2022-10-05 19:38:51 +0300

    initial solution of stage 2

--- src/main/java/ok/dht/test/mikhaylov/MyService.java
+++ src/main/java/ok/dht/test/mikhaylov/MyService.java
@@ -3,6 +3,7 @@ package ok.dht.test.mikhaylov;
 import ok.dht.Service;
 import ok.dht.ServiceConfig;
 import ok.dht.test.ServiceFactory;
+import one.nio.http.HttpException;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
@@ -19,11 +20,15 @@ import org.rocksdb.RocksDBException;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.NoSuchElementException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class MyService implements Service {
-
-    private static final byte[] EMPTY_ID_RESPONSE_BODY = strToBytes("Empty id");
 
     private final ServiceConfig config;
 
@@ -31,33 +36,31 @@ public class MyService implements Service {
 
     private RocksDB db;
 
+    private ExecutorService requestHandlers;
+
+    private static final byte[] EMPTY_ID_RESPONSE_BODY = strToBytes("Empty id");
+    private static final int REQUEST_HANDLERS = 4;
+    private static final int MAX_REQUESTS = 1024;
+
     public MyService(ServiceConfig config) {
         this.config = config;
     }
 
     @Override
     public CompletableFuture<?> start() throws IOException {
+        requestHandlers = new ThreadPoolExecutor(
+                REQUEST_HANDLERS,
+                REQUEST_HANDLERS,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingDeque<>(MAX_REQUESTS)
+        );
         try {
             db = RocksDB.open(config.workingDir().toString());
         } catch (RocksDBException e) {
             throw new IOException(e);
         }
-        server = new HttpServer(createServerConfigFromPort(config.selfPort())) {
-            @Override
-            public void handleDefault(Request request, HttpSession session) throws IOException {
-                session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
-            }
-
-            @Override
-            public synchronized void stop() {
-                for (SelectorThread selectorThread : selectors) {
-                    for (Session session : selectorThread.selector) {
-                        session.close();
-                    }
-                }
-                super.stop();
-            }
-        };
+        server = new MyHttpServer(createServerConfigFromPort(config.selfPort()));
         server.addRequestHandlers(this);
         server.start();
         return CompletableFuture.completedFuture(null);
@@ -76,6 +79,7 @@ public class MyService implements Service {
     public CompletableFuture<?> stop() throws IOException {
         server.stop();
         server = null;
+        requestHandlers.shutdownNow();
         try {
             db.closeE();
         } catch (RocksDBException e) {
@@ -135,12 +139,63 @@ public class MyService implements Service {
         }
     }
 
-    @ServiceFactory(stage = 1, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
+    @ServiceFactory(stage = 2, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
     public static class Factory implements ServiceFactory.Factory {
 
         @Override
         public Service create(ServiceConfig config) {
             return new MyService(config);
+        }
+    }
+
+    private class MyHttpServer extends HttpServer {
+        public MyHttpServer(HttpServerConfig config) throws IOException {
+            super(config);
+        }
+
+        @Override
+        public void handleRequest(Request request, HttpSession session) throws IOException {
+            try {
+                requestHandlers.submit(() -> {
+                    try {
+                        super.handleRequest(request, session);
+                    } catch (Exception e) {
+                        handleRequestException(e, session);
+                    }
+                });
+            } catch (RejectedExecutionException ignored) {
+                session.sendError(Response.SERVICE_UNAVAILABLE, "Server is overloaded");
+            }
+        }
+
+        public void handleRequestException(Exception e, HttpSession session) {
+            try {
+                // missing required parameter
+                if (e instanceof HttpException && e.getCause() instanceof NoSuchElementException) {
+                    session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+                } else {
+                    session.sendError(Response.INTERNAL_ERROR, e.getMessage());
+                }
+            } catch (IOException ex) {
+                RuntimeException re = new RuntimeException(ex);
+                re.addSuppressed(e);
+                throw re;
+            }
+        }
+
+        @Override
+        public void handleDefault(Request request, HttpSession session) throws IOException {
+            session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+        }
+
+        @Override
+        public synchronized void stop() {
+            for (SelectorThread selectorThread : selectors) {
+                for (Session session : selectorThread.selector) {
+                    session.close();
+                }
+            }
+            super.stop();
         }
     }
 }
