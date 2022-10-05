a75a9098c9ddd419d5fd074bd998629287b12ce1
Brat-vseznamus <fvixnin@gmail.com>
2022-10-05 19:44:58 +0300

    fix code style

--- src/main/java/ok/dht/test/vihnin/database/DataBase.java
+++ src/main/java/ok/dht/test/vihnin/database/DataBase.java
@@ -1,8 +1,11 @@
 package ok.dht.test.vihnin.database;
 
 public interface DataBase<K, V> {
+
     V get(K key);
+
     boolean put(K key, V value);
+
     boolean delete(K key);
 
     void close();
