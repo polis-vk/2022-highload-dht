6b7be1dabeb2cdf1ad5560acde9d23cfdfd214af
Aleksey Shik <alexey.shik@corp.mail.ru>
2022-10-05 22:18:28 +0300

    minor fixes

--- src/main/java/ok/dht/test/shik/CustomHttpServer.java
+++ src/main/java/ok/dht/test/shik/CustomHttpServer.java
@@ -14,11 +14,18 @@ import org.apache.commons.logging.LogFactory;
 
 import java.io.IOException;
 import java.nio.BufferOverflowException;
+import java.util.Set;
+import java.util.concurrent.RejectedExecutionException;
 
 public class CustomHttpServer extends HttpServer {
 
     private static final String CLOSE_CONNECTION_HEADER = "Connection: close";
     private static final Log LOG = LogFactory.getLog(CustomHttpServer.class);
+    private static final Set<Integer> SUPPORTED_METHODS = Set.of(
+        Request.METHOD_GET,
+        Request.METHOD_PUT,
+        Request.METHOD_DELETE
+    );
 
     private final WorkersService workersService;
 
@@ -36,28 +43,40 @@ public class CustomHttpServer extends HttpServer {
     }
 
     @Override
-    public void handleRequest(Request request, HttpSession session) {
-        workersService.submitTask(() -> {
-            try {
-                super.handleRequest(request, session);
-            } catch (Exception e) {
-                LOG.error("Error while processing request: ", e);
+    public void handleRequest(Request request, HttpSession session) throws IOException {
+        try {
+            workersService.submitTask(() -> processClientRequest(request, session));
+        } catch (RejectedExecutionException e) {
+            LOG.warn("Internal executor queue is full", e);
+            session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE, Response.EMPTY));
+        }
+    }
 
-                try {
-                    String response = e.getClass() == BufferOverflowException.class
-                        ? Response.REQUEST_ENTITY_TOO_LARGE
-                        : Response.BAD_REQUEST;
-                    session.sendError(response, e.getMessage());
-                } catch (IOException e1) {
-                    LOG.error("Error while sending message about error: ", e1);
-                }
-            }
-        });
+    private void processClientRequest(Request request, HttpSession session) {
+        try {
+            super.handleRequest(request, session);
+        } catch (Exception e) {
+            LOG.error("Error while processing request: ", e);
+            sendErrorToSession(session, e);
+        }
+    }
+
+    private void sendErrorToSession(HttpSession session, Exception e) {
+        try {
+            String response = e.getClass() == BufferOverflowException.class
+                ? Response.REQUEST_ENTITY_TOO_LARGE
+                : Response.BAD_REQUEST;
+            session.sendError(response, e.getMessage());
+        } catch (IOException e1) {
+            LOG.error("Error while sending message about error: ", e1);
+        }
     }
 
     @Override
     public void handleDefault(Request request, HttpSession session) throws IOException {
-        Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
+        Response response = SUPPORTED_METHODS.contains(request.getMethod())
+            ? new Response(Response.BAD_REQUEST, Response.EMPTY)
+            : new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
         session.sendResponse(response);
     }
 
