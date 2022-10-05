34871eab18411e0d41b75483bab17831d768c6b2
Ilin <fmyar@yandex-team.ru>
2022-10-05 21:02:26 +0300

    add solve for stage2 without report

---
+++ src/main/java/ok/dht/test/ilin/servers/ExpandableHttpServer.java
@@ -0,0 +1,81 @@
+package ok.dht.test.ilin.servers;
+
+import ok.dht.test.ilin.config.ExpandableHttpServerConfig;
+import one.nio.http.HttpServer;
+import one.nio.http.HttpSession;
+import one.nio.http.Request;
+import one.nio.http.Response;
+import one.nio.net.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ExpandableHttpServer extends HttpServer {
+    private final ExecutorService executorService;
+    private final LinkedBlockingQueue<Runnable> queue;
+    private final ExpandableHttpServerConfig config;
+    private final Logger logger = LoggerFactory.getLogger(ExpandableHttpServer.class);
+
+    public ExpandableHttpServer(ExpandableHttpServerConfig config, Object... routers) throws IOException {
+        super(config, routers);
+        this.config = config;
+        this.queue = new LinkedBlockingQueue<>(config.queueCapacity);
+        this.executorService = new ThreadPoolExecutor(
+            config.workers,
+            config.workers,
+            0L,
+            TimeUnit.MILLISECONDS,
+            this.queue
+        );
+    }
+
+    @Override
+    public void handleDefault(Request request, HttpSession session) throws IOException {
+        session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+    }
+
+    @Override
+    public void handleRequest(Request request, HttpSession session) throws IOException {
+        if (queue.size() >= config.queueCapacity) {
+            sendServiceUnavailable(session);
+        }
+        try {
+            executorService.execute(() -> {
+                try {
+                    super.handleRequest(request, session);
+                } catch (Exception e) {
+                    logger.error("failed to handle request: {}", e.getMessage());
+                    try {
+                        session.sendError(Response.BAD_REQUEST, "failed to execute request.");
+                    } catch (IOException ex) {
+                        logger.error("failed to send error: {}", e.getMessage());
+                    }
+                }
+            });
+        } catch (RejectedExecutionException e) {
+            logger.error("Failed to run execution: {}", e.getMessage());
+            sendServiceUnavailable(session);
+        }
+    }
+
+    private void sendServiceUnavailable(HttpSession session) {
+        try {
+            session.sendResponse(new Response(Response.SERVICE_UNAVAILABLE, Response.EMPTY));
+        } catch (IOException e) {
+            logger.error("Failed to send response: {}", e.getMessage());
+        }
+    }
+
+    @Override
+    public synchronized void stop() {
+        Arrays.stream(selectors).forEach(it -> it.selector.forEach(Session::close));
+        super.stop();
+    }
+}
