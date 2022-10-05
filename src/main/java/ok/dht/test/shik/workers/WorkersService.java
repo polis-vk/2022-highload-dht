844f45b584a2e4e0f06e54bfa060bbca38b42d28
Aleksey Shik <alexey.shik@corp.mail.ru>
2022-10-05 23:38:01 +0300

    minor fixes

--- src/main/java/ok/dht/test/shik/workers/WorkersService.java
+++ src/main/java/ok/dht/test/shik/workers/WorkersService.java
@@ -1,5 +1,8 @@
 package ok.dht.test.shik.workers;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -11,7 +14,8 @@ import java.util.concurrent.TimeUnit;
 
 public class WorkersService {
 
-    private static final int TIMEOUT_MILLIS = 10;
+    private static final Log LOG = LogFactory.getLog(WorkersService.class);
+    private static final int TIMEOUT_MILLIS = 100;
 
     private ExecutorService pool;
     private final WorkersConfig config;
@@ -30,12 +34,21 @@ public class WorkersService {
             Executors.defaultThreadFactory(), rejectedHandler);
     }
 
+    // В данный момент мой HttpServer не использует возвращаемое значение
+    // Но глобально оно может иметь смысл, например, если критически важно eventually выполнить запросы,
+    // то можно сохранить их в бд и исполнить при следующем старте сервера
     public List<Runnable> stop() {
         pool.shutdown();
         try {
-            return pool.awaitTermination(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS)
-                ? Collections.emptyList()
-                : pool.shutdownNow();
+            if (!pool.awaitTermination(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS)) {
+                List<Runnable> uncompletedTasks = pool.shutdownNow();
+                if (!pool.awaitTermination(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS)) {
+                    LOG.warn("Cannot terminate thread pool");
+                }
+                return uncompletedTasks;
+            }
+
+            return Collections.emptyList();
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             return pool.shutdownNow();
