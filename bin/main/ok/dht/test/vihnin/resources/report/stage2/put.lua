9632ff22df302c252fc7c186d8cfc0393c730f58
Brat-vseznamus <fvixnin@gmail.com>
2022-10-05 23:59:36 +0300

    scripts

---
+++ bin/main/ok/dht/test/vihnin/resources/report/stage2/put.lua
@@ -0,0 +1,12 @@
+math.randomseed(os.time())
+
+-- for few threads
+
+function request()
+    return wrk.format(
+        "PUT", 
+        "/v0/entity?id=" .. math.random(1, 1024 * 1024),
+        {}, 
+        string.rep("data", 100)
+    )
+end
