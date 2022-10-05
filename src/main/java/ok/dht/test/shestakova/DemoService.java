06acf1a63251508589a2273a1e226c1ecdc9398b
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2022-10-05 22:20:14 +0300

    Fixed due to a new test

--- src/main/java/ok/dht/test/shestakova/DemoService.java
+++ src/main/java/ok/dht/test/shestakova/DemoService.java
@@ -63,7 +63,15 @@ public class DemoService implements Service {
 
             @Override
             public void handleDefault(Request request, HttpSession session) throws IOException {
-                Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
+                Response response;
+                int requestMethod = request.getMethod();
+                if (requestMethod == Request.METHOD_GET
+                        || requestMethod == Request.METHOD_PUT
+                        || requestMethod == Request.METHOD_DELETE) {
+                    response = new Response(Response.BAD_REQUEST, Response.EMPTY);
+                } else {
+                    response = new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
+                }
                 session.sendResponse(response);
             }
         };
