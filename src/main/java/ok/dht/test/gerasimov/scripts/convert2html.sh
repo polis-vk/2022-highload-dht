20b477e0ccdd66cb68d16a5b75a62fda3dcb7e13
Mizzza54 <gerasimov.misha2001@gmail.com>
2022-10-05 22:00:34 +0300

    STAGE-2: Update script

--- src/main/java/ok/dht/test/gerasimov/scripts/convert2html.sh
+++ src/main/java/ok/dht/test/gerasimov/scripts/convert2html.sh
@@ -23,13 +23,25 @@ function run_jfr2flame_cpu() {
   cd "$path" || exit
 }
 
-rates=(1000 3000 5000 8000 12000 15000 20000)
+function run_jfr2flame_lock() {
+  local src_name=$1
+  local dst_name=$2
+  local src_path="$PWD/$src_name"
+  local dst_path="$PWD/$dst_name"
+  local path=$PWD
+  cd $path_to_async_profiler || exit
+  java -cp "$path_to_async_profiler/build/converter.jar" jfr2heat --lock "$src_path" "$dst_path"
+  cd "$path" || exit
+}
+
+rates=(5000)
 
 for rate in "${rates[@]}"; do
   type_request="put"
   file_name="async-profiler_${type_request}_rate_${rate}.jfr"
   run_jfr2flame_alloc "$file_name" "async-profiler_${type_request}_alloc_rate_${rate}.html"
   run_jfr2flame_cpu "$file_name" "async-profiler_${type_request}_cpu_rate_${rate}.html"
+#  run_jfr2flame_lock "$file_name" "async-profiler_${type_request}_lock_rate_${rate}.html"
 done
 
 for rate in "${rates[@]}"; do
@@ -37,4 +49,5 @@ for rate in "${rates[@]}"; do
   file_name="async-profiler_${type_request}_rate_${rate}.jfr"
   run_jfr2flame_alloc "$file_name" "async-profiler_${type_request}_alloc_rate_${rate}.html"
   run_jfr2flame_cpu "$file_name" "async-profiler_${type_request}_cpu_rate_${rate}.html"
+#  run_jfr2flame_lock "$file_name" "async-profiler_${type_request}_lock_rate_${rate}.html"
 done
