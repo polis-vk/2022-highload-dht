44932264e852b0528eb59d0b3c7454946abc44d9
Alexander Kiselyov <a_kiselyov-snz@mail.ru>
2022-10-05 03:25:38 +0300

    add report

--- src/main/java/ok/dht/test/kiselyov/WebService.java
+++ src/main/java/ok/dht/test/kiselyov/WebService.java
@@ -25,7 +25,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
--- src/main/java/ok/dht/test/kiselyov/util/CustomLinkedBlockingDeque.java
+++ src/main/java/ok/dht/test/kiselyov/util/CustomLinkedBlockingDeque.java
@@ -1,11 +1,8 @@
 package ok.dht.test.kiselyov.util;
 
 import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CustomLinkedBlockingDeque<E> extends LinkedBlockingDeque<E> {
-
-    private final AtomicBoolean isHead = new AtomicBoolean(true);
 
     public CustomLinkedBlockingDeque(int dequeCapacity) {
         super(dequeCapacity);
@@ -14,12 +11,5 @@ public class CustomLinkedBlockingDeque<E> extends LinkedBlockingDeque<E> {
     @Override
     public E take() throws InterruptedException {
         return super.takeLast();
-        /*if (isHead.get()) {
-            isHead.set(!isHead.get());
-            return super.takeFirst();
-        } else {
-            isHead.set(!isHead.get());
-            return super.takeLast();
-        }*/
     }
 }
