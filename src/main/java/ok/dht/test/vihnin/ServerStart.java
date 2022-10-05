bd8c6026682650f2b23a0acc532371156eeb20fd
Brat-vseznamus <fvixnin@gmail.com>
2022-10-05 19:44:58 +0300

    fix code style

--- src/main/java/ok/dht/test/vihnin/ServerStart.java
+++ src/main/java/ok/dht/test/vihnin/ServerStart.java
@@ -1,22 +1,25 @@
 package ok.dht.test.vihnin;
 
 import ok.dht.ServiceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class ServerStart {
+public final class ServerStart {
+    private static final Logger logger = LoggerFactory.getLogger(ServerStart.class);
 
     private ServerStart() {
-        
+
     }
 
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    public static void main(String[] args)
+            throws IOException, ExecutionException, InterruptedException, TimeoutException {
         int port = 19234;
         String url = "http://localhost:" + port;
         ServiceConfig cfg = new ServiceConfig(
@@ -26,6 +29,6 @@ public class ServerStart {
                 Path.of("/home/fedor/Documents/uni-data/highload/trash")
         );
         new HighLoadService(cfg).start().get(1, TimeUnit.SECONDS);
-        System.out.println("[SERVER START] Socket is ready: " + url);
+        logger.info("[SERVER START] Socket is ready: " + url);
     }
 }
