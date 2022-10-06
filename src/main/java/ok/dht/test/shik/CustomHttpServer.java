5c4ecf3a8ca2bc6d599ea0c7cb70b103f154e176
Aleksey Shik <alexey.shik@corp.mail.ru>
2022-10-06 22:51:49 +0300

    fix new test

--- src/main/java/ok/dht/test/shik/CustomHttpServer.java
+++ src/main/java/ok/dht/test/shik/CustomHttpServer.java
@@ -11,11 +11,17 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import java.io.IOException;
+import java.util.Set;
 
 public class CustomHttpServer extends HttpServer {
 
     private static final String CLOSE_CONNECTION_HEADER = "Connection: close";
     private static final Log LOG = LogFactory.getLog(CustomHttpServer.class);
+    private static final Set<Integer> SUPPORTED_METHODS = Set.of(
+        Request.METHOD_GET,
+        Request.METHOD_PUT,
+        Request.METHOD_DELETE
+    );
 
     public CustomHttpServer(HttpServerConfig config, Object... routers) throws IOException {
         super(config, routers);
@@ -23,7 +29,9 @@ public class CustomHttpServer extends HttpServer {
 
     @Override
     public void handleDefault(Request request, HttpSession session) throws IOException {
-        Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
+        Response response = SUPPORTED_METHODS.contains(request.getMethod())
+            ? new Response(Response.BAD_REQUEST, Response.EMPTY)
+            : new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
         session.sendResponse(response);
     }
 
