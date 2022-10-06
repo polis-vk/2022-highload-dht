f239771f2aed96c1f3fb9c7b3ea4f38dfd4a5bfd
PaaavelZ <paaavel.2024@gmail.com>
2022-10-06 03:58:42 +0300

    Fix codeclimate 2

--- src/main/java/ok/dht/kovalenko/dao/utils/FileUtils.java
+++ src/main/java/ok/dht/kovalenko/dao/utils/FileUtils.java
@@ -8,7 +8,6 @@ import java.io.UncheckedIOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import java.util.regex.Matcher;
