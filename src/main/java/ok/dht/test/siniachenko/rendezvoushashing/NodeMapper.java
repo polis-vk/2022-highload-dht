8fb66e343bc7167e2e67cf29e8b34eb84d441349
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-10-07 18:41:27 +0300

    nodes started requesting other nodes

---
+++ src/main/java/ok/dht/test/siniachenko/rendezvoushashing/NodeMapper.java
@@ -0,0 +1,29 @@
+package ok.dht.test.siniachenko.rendezvoushashing;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class NodeMapper {
+    private final List<String> nodeUrls;
+
+    public NodeMapper(List<String> nodeUrls) {
+        this.nodeUrls = nodeUrls;
+    }
+
+    public String getNodeUrlByKey(byte[] key) {
+        int maxHash = Integer.MIN_VALUE;
+        String maxHashNodeUrl = nodeUrls.get(0);
+        for (final String nodeUrl : nodeUrls) {
+            int tempHash = hash(key, nodeUrl);
+            if (maxHash < tempHash) {
+                maxHash = tempHash;
+                maxHashNodeUrl = nodeUrl;
+            }
+        }
+        return maxHashNodeUrl;
+    }
+
+    private static int hash(byte[] key, String node) {
+        return Arrays.hashCode(key) * 1009 + node.hashCode();
+    }
+}
--- src/main/java/ok/dht/test/siniachenko/service/TycoonService.java
+++ src/main/java/ok/dht/test/siniachenko/service/TycoonService.java
@@ -3,6 +3,7 @@ package ok.dht.test.siniachenko.service;
 import ok.dht.ServiceConfig;
 import ok.dht.test.ServiceFactory;
 import ok.dht.test.siniachenko.TycoonHttpServer;
+import ok.dht.test.siniachenko.rendezvoushashing.NodeMapper;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpSession;
 import one.nio.http.Request;
@@ -16,7 +17,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.util.concurrent.*;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 public class TycoonService implements ok.dht.Service {
@@ -26,19 +36,25 @@ public class TycoonService implements ok.dht.Service {
     public static final int THREAD_POOL_QUEUE_CAPACITY = (int) 1E20;
 
     private final ServiceConfig config;
+    private final NodeMapper nodeMapper;
     private DB levelDb;
     private HttpServer server;
     private ExecutorService executorService;
+    private HttpClient httpClient;
 
     public TycoonService(ServiceConfig config) {
         this.config = config;
+        this.nodeMapper = new NodeMapper(config.clusterUrls());
     }
 
     @Override
     public CompletableFuture<?> start() throws IOException {
+        // DB
+        System.out.println("DIRECTORY ___________ " + config.workingDir().toString());
         levelDb = new DbImpl(new Options(), config.workingDir().toFile());
         LOG.info("Started DB in directory {}", config.workingDir());
 
+        // Executor
         int threadPoolSize = AVAILABLE_PROCESSORS;
         executorService = new ThreadPoolExecutor(
             threadPoolSize, threadPoolSize,
@@ -46,6 +62,10 @@ public class TycoonService implements ok.dht.Service {
             new LinkedBlockingQueue<>(THREAD_POOL_QUEUE_CAPACITY)
         );
 
+        // Http Client
+        httpClient = HttpClient.newHttpClient();
+
+        // Http Server
         server = new TycoonHttpServer(this, config.selfPort());
         server.start();
         LOG.info("Service started on {}, executor threads: {}", config.selfUrl(), threadPoolSize);
@@ -55,9 +75,16 @@ public class TycoonService implements ok.dht.Service {
 
     @Override
     public CompletableFuture<?> stop() throws IOException {
-        server.stop();
-        levelDb.close();
-        executorService.shutdown();
+        if (server != null) {
+            server.stop();
+        }
+        if (executorService != null) {
+            executorService.shutdown();
+        }
+        if (levelDb != null) {
+            levelDb.close();
+        }
+
         return CompletableFuture.completedFuture(null);
     }
 
@@ -74,9 +101,9 @@ public class TycoonService implements ok.dht.Service {
         }
 
         switch (request.getMethod()) {
-            case Request.METHOD_GET -> execute(session, () -> getEntity(idParameter));
-            case Request.METHOD_PUT -> execute(session, () -> upsertEntity(idParameter, request));
-            case Request.METHOD_DELETE -> execute(session, () -> deleteEntity(idParameter));
+            case Request.METHOD_GET -> execute(session, () -> get(idParameter, request));
+            case Request.METHOD_PUT -> execute(session, () -> upsert(idParameter, request));
+            case Request.METHOD_DELETE -> execute(session, () -> delete(idParameter, request));
             default -> execute(session, () -> new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY));
         }
     }
@@ -104,57 +131,101 @@ public class TycoonService implements ok.dht.Service {
         }
     }
 
-    private Response getEntity(String id) {
-        byte[] value;
-        try {
-            value = levelDb.get(Utf8.toBytes(id));
-        } catch (DBException e) {
-            LOG.error("Error in DB", e);
-            return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+    private Response get(String id, Request request) {
+        byte[] value = Response.EMPTY;
+
+        String nodeUrlByKey = nodeMapper.getNodeUrlByKey(Utf8.toBytes(id));
+        if (config.selfUrl().equals(nodeUrlByKey)) {
+            try {
+                value = levelDb.get(Utf8.toBytes(id));
+            } catch (DBException e) {
+                LOG.error("Error in DB", e);
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
+        } else {
+            try {
+                HttpResponse<byte[]> response = httpClient.send(
+                    HttpRequest.newBuilder()
+                        .GET()
+                        .uri(URI.create(nodeUrlByKey + REQUESTS_PATH + "?id=" + id))
+                        .build(),
+                    HttpResponse.BodyHandlers.ofByteArray()
+                );
+                value = response.body();
+            } catch (IOException | InterruptedException e) {
+                LOG.error("Error while requesting node {}", nodeUrlByKey, e);
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
         }
 
         if (value == null) {
-            return new Response(
-                Response.NOT_FOUND,
-                Response.EMPTY
-            );
+            return new Response(Response.NOT_FOUND, Response.EMPTY);
         } else {
-            return new Response(
-                Response.OK,
-                value
-            );
+            return new Response(Response.OK, value);
         }
     }
 
-    private Response upsertEntity(String id, Request request) {
-        try {
-            levelDb.put(Utf8.toBytes(id), request.getBody());
-        } catch (DBException e) {
-            LOG.error("Error in DB", e);
-            return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+    private Response upsert(String id, Request request) {
+        byte[] value = Response.EMPTY;
+
+        String nodeUrlByKey = nodeMapper.getNodeUrlByKey(Utf8.toBytes(id));
+        if (config.selfUrl().equals(nodeUrlByKey)) {
+            try {
+                levelDb.put(Utf8.toBytes(id), request.getBody());
+            } catch (DBException e) {
+                LOG.error("Error in DB", e);
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
+        } else {
+            try {
+                HttpResponse<byte[]> response = httpClient.send(
+                    HttpRequest.newBuilder()
+                        .PUT(HttpRequest.BodyPublishers.ofByteArray(request.getBody()))
+                        .uri(URI.create(nodeUrlByKey + REQUESTS_PATH + "?id=" + id))
+                        .build(),
+                    HttpResponse.BodyHandlers.ofByteArray()
+                );
+                value = response.body();
+            } catch (IOException | InterruptedException e) {
+                LOG.error("Error while requesting node {}", nodeUrlByKey, e);
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
         }
 
-        return new Response(
-            Response.CREATED,
-            Response.EMPTY
-        );
+        return new Response(Response.CREATED, value);
     }
 
-    private Response deleteEntity(String id) {
-        try {
-            levelDb.delete(Utf8.toBytes(id));
-        } catch (DBException e) {
-            LOG.error("Error in DB", e);
-            return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+    private Response delete(String id, Request request) {
+        byte[] value = Response.EMPTY;
+
+        String nodeUrlByKey = nodeMapper.getNodeUrlByKey(Utf8.toBytes(id));
+        if (config.selfUrl().equals(nodeUrlByKey)) {
+            try {
+                levelDb.delete(Utf8.toBytes(id));
+            } catch (DBException e) {
+                LOG.error("Error in DB", e);
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
+        } else {
+            try {
+                HttpResponse<byte[]> response = httpClient.send(
+                    HttpRequest.newBuilder()
+                        .DELETE()
+                        .uri(URI.create(nodeUrlByKey + REQUESTS_PATH + "?id=" + id))
+                        .build(),
+                    HttpResponse.BodyHandlers.ofByteArray()
+                );
+                value = response.body();
+            } catch (IOException | InterruptedException e) {
+                LOG.error("Error while requesting node {}", nodeUrlByKey, e);
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
         }
 
-        return new Response(
-            Response.ACCEPTED,
-            Response.EMPTY
-        );
+        return new Response(Response.ACCEPTED, value);
     }
 
-    @ServiceFactory(stage = 2, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
+    @ServiceFactory(stage = 3, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
     public static class Factory implements ServiceFactory.Factory {
         @Override
         public ok.dht.Service create(ServiceConfig config) {
