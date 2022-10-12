5bfe6526d4c15332cf1f68a2f6dceb88f768c788
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-10-12 20:29:00 +0300

    TycoonServer: added parsing CLUSTER_URL and URL

--- src/main/java/ok/dht/test/siniachenko/TycoonServer.java
+++ src/main/java/ok/dht/test/siniachenko/TycoonServer.java
@@ -7,7 +7,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -20,21 +21,50 @@ public final class TycoonServer {
 
     public static void main(String[] args)
         throws IOException, ExecutionException, InterruptedException, TimeoutException {
-        int port = 12345;
-        String url = String.format("http://localhost:%d", port);
+        String url = getUrl();
+        List<String> clusterUrls = getClusterUrls(url);
+        if (!clusterUrls.contains(url)) {
+            throw new IllegalArgumentException(
+                "Cluster urls [" + String.join(",", clusterUrls) + "] don't contain our URL " + url
+            );
+        }
         ServiceConfig config;
         try {
             config = new ServiceConfig(
-                port,
+                Integer.parseInt(url.split(":")[2]),
                 url,
-                Collections.singletonList(url),
+                clusterUrls,
                 Files.createTempDirectory("server")
             );
         } catch (IOException e) {
             LOG.error("Cannot create server directory \"server\"");
             throw e;
         }
-        new TycoonService(config).start().get(1, TimeUnit.SECONDS);
+        TycoonService tycoonService = new TycoonService(config);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                tycoonService.stop().get(1, TimeUnit.SECONDS);
+            } catch (InterruptedException | ExecutionException | TimeoutException | IOException e) {
+                LOG.error("Error while stopping server", e);
+            }
+        }));
+        tycoonService.start().get(1, TimeUnit.SECONDS);
         LOG.info("Started Server on {}", url);
+    }
+
+    private static String getUrl() {
+        String urlEnv = System.getenv("URL");
+        if (urlEnv == null) {
+            urlEnv = String.format("http://localhost:%d", 12345);
+        }
+        return urlEnv;
+    }
+
+    private static List<String> getClusterUrls(String ourUrl) {
+        String clusterUrlsEnv = System.getenv("CLUSTER_URLS");
+        if (clusterUrlsEnv == null) {
+            clusterUrlsEnv = ourUrl;
+        }
+        return Arrays.asList(clusterUrlsEnv.split(","));
     }
 }
