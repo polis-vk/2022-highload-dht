e4683703dc168c6391549b0e525efc9440265069
ralerculte <ralerculte@yandex.ru>
2022-10-15 18:56:15 +0300

    Refactor handle requests

--- src/main/java/ok/dht/test/komissarov/CourseService.java
+++ src/main/java/ok/dht/test/komissarov/CourseService.java
@@ -1,34 +1,18 @@
 package ok.dht.test.komissarov;
 
-import jdk.incubator.foreign.MemorySegment;
 import ok.dht.Service;
 import ok.dht.ServiceConfig;
 import ok.dht.test.ServiceFactory;
-import ok.dht.test.komissarov.database.MemorySegmentDao;
-import ok.dht.test.komissarov.database.models.BaseEntry;
-import ok.dht.test.komissarov.database.models.Config;
-import ok.dht.test.komissarov.database.models.Entry;
 import ok.dht.test.komissarov.utils.CustomHttpServer;
 import one.nio.http.HttpServer;
-import one.nio.http.HttpServerConfig;
-import one.nio.http.Param;
-import one.nio.http.Path;
-import one.nio.http.Request;
-import one.nio.http.RequestMethod;
-import one.nio.http.Response;
-import one.nio.server.AcceptorConfig;
-import one.nio.util.Utf8;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 
 public class CourseService implements Service {
 
-    private static final String PATH = "/v0/entity";
-
     private final ServiceConfig config;
     private HttpServer server;
-    private MemorySegmentDao dao;
 
     public CourseService(ServiceConfig config) {
         this.config = config;
@@ -36,86 +20,15 @@ public class CourseService implements Service {
 
     @Override
     public CompletableFuture<?> start() throws IOException {
-        dao = new MemorySegmentDao(new Config(
-                config.workingDir(),
-                1 << 20
-        ));
-        server = new CustomHttpServer(createConfigFromPort(config.selfPort()));
-        server.addRequestHandlers(this);
+        server = new CustomHttpServer(config);
         server.start();
         return CompletableFuture.completedFuture(null);
     }
 
-    @Path(PATH)
-    @RequestMethod(Request.METHOD_GET)
-    public Response findById(@Param(value = "id") String id) {
-        if (id == null || id.isEmpty()) {
-            return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        }
-
-        Entry<MemorySegment> entry = dao.get(fromString(id));
-        if (entry == null) {
-            return new Response(Response.NOT_FOUND, Response.EMPTY);
-        }
-        return new Response(Response.OK, entry.value().toByteArray());
-    }
-
-    @Path(PATH)
-    @RequestMethod(Request.METHOD_PUT)
-    public Response persist(@Param(value = "id") String id,
-                            Request request) {
-        if (id == null || id.isEmpty()) {
-            return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        }
-
-        Entry<MemorySegment> entry = new BaseEntry<>(
-                fromString(id),
-                MemorySegment.ofArray(request.getBody())
-        );
-        dao.upsert(entry);
-        return new Response(Response.CREATED, Response.EMPTY);
-    }
-
-    @Path(PATH)
-    @RequestMethod(Request.METHOD_DELETE)
-    public Response delete(@Param(value = "id") String id) {
-        if (id == null || id.isEmpty()) {
-            return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        }
-
-        Entry<MemorySegment> removedEntry = new BaseEntry<>(
-                fromString(id),
-                null
-        );
-        dao.upsert(removedEntry);
-        return new Response(Response.ACCEPTED, Response.EMPTY);
-    }
-
-    @Path(PATH)
-    @RequestMethod(Request.METHOD_POST)
-    public Response post() {
-        // NOT IMPLEMENTED
-        return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
-    }
-
     @Override
-    public CompletableFuture<?> stop() throws IOException {
+    public CompletableFuture<?> stop() {
         server.stop();
-        dao.close();
         return CompletableFuture.completedFuture(null);
-    }
-
-    private static HttpServerConfig createConfigFromPort(int port) {
-        HttpServerConfig httpConfig = new HttpServerConfig();
-        AcceptorConfig acceptor = new AcceptorConfig();
-        acceptor.port = port;
-        acceptor.reusePort = true;
-        httpConfig.acceptors = new AcceptorConfig[]{acceptor};
-        return httpConfig;
-    }
-
-    private static MemorySegment fromString(String value) {
-        return value == null ? null : MemorySegment.ofArray(Utf8.toBytes(value));
     }
 
     @ServiceFactory(stage = 2, week = 1)
--- src/main/java/ok/dht/test/komissarov/utils/CustomHttpServer.java
+++ src/main/java/ok/dht/test/komissarov/utils/CustomHttpServer.java
@@ -1,12 +1,20 @@
 package ok.dht.test.komissarov.utils;
 
+import jdk.incubator.foreign.MemorySegment;
+import ok.dht.ServiceConfig;
+import ok.dht.test.komissarov.database.MemorySegmentDao;
+import ok.dht.test.komissarov.database.models.BaseEntry;
+import ok.dht.test.komissarov.database.models.Config;
+import ok.dht.test.komissarov.database.models.Entry;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
 import one.nio.http.Request;
 import one.nio.http.Response;
 import one.nio.net.Session;
+import one.nio.server.AcceptorConfig;
 import one.nio.server.SelectorThread;
+import one.nio.util.Utf8;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,44 +26,112 @@ import java.util.concurrent.TimeUnit;
 
 public class CustomHttpServer extends HttpServer {
 
+    private static final String PATH = "/v0/entity";
+    private static final String PARAM_KEY = "id=";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomHttpServer.class);
 
-    // hardcoded params -> fix
+    private final MemorySegmentDao dao;
     private final ExecutorService executorService = new ThreadPoolExecutor(
             4,
-            8,
+            6,
             30,
             TimeUnit.SECONDS,
             new LinkedBlockingQueue<>()
     );
 
-    public CustomHttpServer(HttpServerConfig config, Object... routers) throws IOException {
-        super(config, routers);
+    public CustomHttpServer(ServiceConfig config) throws IOException {
+        super(createConfigFromPort(config.selfPort()));
+        dao = new MemorySegmentDao(new Config(
+                config.workingDir(),
+                1 << 20
+        ));
     }
 
     @Override
-    public void handleDefault(Request request, HttpSession session) throws IOException {
-        Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
-        session.sendResponse(response);
+    public void handleRequest(Request request, HttpSession session) throws IOException {
+        handle(request, session);
     }
 
     @Override
-    public void handleRequest(Request request, HttpSession session) {
+    public synchronized void stop() {
+        try {
+            executorService.shutdown();
+            super.stop();
+            for (SelectorThread thread : selectors) {
+                thread.selector.forEach(Session::close);
+            }
+            dao.close();
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    private static MemorySegment fromString(String value) {
+        return value == null ? null : MemorySegment.ofArray(Utf8.toBytes(value));
+    }
+
+    private static HttpServerConfig createConfigFromPort(int port) {
+        HttpServerConfig httpConfig = new HttpServerConfig();
+        AcceptorConfig acceptor = new AcceptorConfig();
+        acceptor.port = port;
+        acceptor.reusePort = true;
+        httpConfig.acceptors = new AcceptorConfig[]{acceptor};
+        return httpConfig;
+    }
+
+    private void handle(Request request, HttpSession session) throws IOException {
+        String path = request.getPath();
+        if (!path.equals(PATH)) {
+            session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+            return;
+        }
+
+        String id = request.getParameter(PARAM_KEY);
+        if (id == null || id.isEmpty()) {
+            session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+            return;
+        }
+
         executorService.execute(() -> {
             try {
-                super.handleRequest(request, session);
+                Response response = getResponse(request, id);
+                session.sendResponse(response);
             } catch (IOException e) {
                 LOGGER.error(e.getMessage());
             }
         });
     }
 
-    @Override
-    public synchronized void stop() {
-        executorService.shutdown();
-        super.stop();
-        for (SelectorThread thread : selectors) {
-            thread.selector.forEach(Session::close);
+    private Response getResponse(Request request, String id) {
+        int method = request.getMethod();
+        switch (method) {
+            case Request.METHOD_GET -> {
+                Entry<MemorySegment> entry = dao.get(fromString(id));
+                if (entry == null) {
+                    return new Response(Response.NOT_FOUND, Response.EMPTY);
+                }
+                return new Response(Response.OK, entry.value().toByteArray());
+            }
+            case Request.METHOD_PUT -> {
+                Entry<MemorySegment> entry = new BaseEntry<>(
+                        fromString(id),
+                        MemorySegment.ofArray(request.getBody())
+                );
+                dao.upsert(entry);
+                return new Response(Response.CREATED, Response.EMPTY);
+            }
+            case Request.METHOD_DELETE -> {
+                Entry<MemorySegment> removedEntry = new BaseEntry<>(
+                        fromString(id),
+                        null
+                );
+                dao.upsert(removedEntry);
+                return new Response(Response.ACCEPTED, Response.EMPTY);
+            }
+            default -> {
+                return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
+            }
         }
     }
 }
