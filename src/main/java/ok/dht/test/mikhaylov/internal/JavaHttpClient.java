6c63894f0bb564d884650cdee37473908c2ff636
Maxim Mikhaylov <mmikhaylov56@gmail.com>
2022-10-18 01:17:33 +0300

    stage 3: fix whitespace

--- src/main/java/ok/dht/test/mikhaylov/internal/JavaHttpClient.java
+++ src/main/java/ok/dht/test/mikhaylov/internal/JavaHttpClient.java
@@ -57,26 +57,26 @@ public class JavaHttpClient extends InternalHttpClient {
 
     private static String responseCodeToStatusText(int code) {
         return switch (code) {
-            case HttpURLConnection.HTTP_OK ->Response.OK;
-            case HttpURLConnection.HTTP_CREATED ->Response.CREATED;
-            case HttpURLConnection.HTTP_ACCEPTED ->Response.ACCEPTED;
-            case HttpURLConnection.HTTP_NO_CONTENT ->Response.NO_CONTENT;
-            case HttpURLConnection.HTTP_SEE_OTHER ->Response.SEE_OTHER;
-            case HttpURLConnection.HTTP_NOT_MODIFIED ->Response.NOT_MODIFIED;
-            case HttpURLConnection.HTTP_USE_PROXY ->Response.USE_PROXY;
-            case HttpURLConnection.HTTP_BAD_REQUEST ->Response.BAD_REQUEST;
-            case HttpURLConnection.HTTP_UNAUTHORIZED ->Response.UNAUTHORIZED;
-            case HttpURLConnection.HTTP_PAYMENT_REQUIRED ->Response.PAYMENT_REQUIRED;
-            case HttpURLConnection.HTTP_FORBIDDEN ->Response.FORBIDDEN;
-            case HttpURLConnection.HTTP_NOT_FOUND ->Response.NOT_FOUND;
-            case HttpURLConnection.HTTP_NOT_ACCEPTABLE ->Response.NOT_ACCEPTABLE;
-            case HttpURLConnection.HTTP_CONFLICT ->Response.CONFLICT;
-            case HttpURLConnection.HTTP_GONE ->Response.GONE;
-            case HttpURLConnection.HTTP_LENGTH_REQUIRED ->Response.LENGTH_REQUIRED;
-            case HttpURLConnection.HTTP_INTERNAL_ERROR ->Response.INTERNAL_ERROR;
-            case HttpURLConnection.HTTP_NOT_IMPLEMENTED ->Response.NOT_IMPLEMENTED;
-            case HttpURLConnection.HTTP_BAD_GATEWAY ->Response.BAD_GATEWAY;
-            case HttpURLConnection.HTTP_GATEWAY_TIMEOUT ->Response.GATEWAY_TIMEOUT;
+            case HttpURLConnection.HTTP_OK -> Response.OK;
+            case HttpURLConnection.HTTP_CREATED -> Response.CREATED;
+            case HttpURLConnection.HTTP_ACCEPTED -> Response.ACCEPTED;
+            case HttpURLConnection.HTTP_NO_CONTENT -> Response.NO_CONTENT;
+            case HttpURLConnection.HTTP_SEE_OTHER -> Response.SEE_OTHER;
+            case HttpURLConnection.HTTP_NOT_MODIFIED -> Response.NOT_MODIFIED;
+            case HttpURLConnection.HTTP_USE_PROXY -> Response.USE_PROXY;
+            case HttpURLConnection.HTTP_BAD_REQUEST -> Response.BAD_REQUEST;
+            case HttpURLConnection.HTTP_UNAUTHORIZED -> Response.UNAUTHORIZED;
+            case HttpURLConnection.HTTP_PAYMENT_REQUIRED -> Response.PAYMENT_REQUIRED;
+            case HttpURLConnection.HTTP_FORBIDDEN -> Response.FORBIDDEN;
+            case HttpURLConnection.HTTP_NOT_FOUND -> Response.NOT_FOUND;
+            case HttpURLConnection.HTTP_NOT_ACCEPTABLE -> Response.NOT_ACCEPTABLE;
+            case HttpURLConnection.HTTP_CONFLICT -> Response.CONFLICT;
+            case HttpURLConnection.HTTP_GONE -> Response.GONE;
+            case HttpURLConnection.HTTP_LENGTH_REQUIRED -> Response.LENGTH_REQUIRED;
+            case HttpURLConnection.HTTP_INTERNAL_ERROR -> Response.INTERNAL_ERROR;
+            case HttpURLConnection.HTTP_NOT_IMPLEMENTED -> Response.NOT_IMPLEMENTED;
+            case HttpURLConnection.HTTP_BAD_GATEWAY -> Response.BAD_GATEWAY;
+            case HttpURLConnection.HTTP_GATEWAY_TIMEOUT -> Response.GATEWAY_TIMEOUT;
             default -> throw new IllegalArgumentException("Unknown response code: " + code);
         };
     }
