b7c0e9578dacf809b823d80fe57e6efcce906b60
Veronika <58723287+interestIngc@users.noreply.github.com>
2022-10-18 20:36:34 +0200

    homework 3 published

--- src/main/java/ok/dht/test/anikina/ServiceImpl.java
+++ src/main/java/ok/dht/test/anikina/ServiceImpl.java
@@ -30,7 +30,7 @@ public class ServiceImpl implements Service {
         return CompletableFuture.completedFuture(null);
     }
 
-    @ServiceFactory(stage = 3, week = 1)
+    @ServiceFactory(stage = 3, week = 2)
     public static class Factory implements ServiceFactory.Factory {
         @Override
         public Service create(ServiceConfig config) {
