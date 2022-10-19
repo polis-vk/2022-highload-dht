8d1a2ac3fd6023e5df268b16771fb054f9296e3d
Ilya Shakhov <46408420+pixcc@users.noreply.github.com>
2022-10-19 19:43:24 +0300

    hw3 initial tests

--- src/main/java/ok/dht/test/shakhov/KeyValueService.java
+++ src/main/java/ok/dht/test/shakhov/KeyValueService.java
@@ -11,10 +11,7 @@ import ok.dht.test.shakhov.dao.Entry;
 import ok.dht.test.shakhov.dao.MemorySegmentDao;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
-import one.nio.http.Param;
-import one.nio.http.Path;
 import one.nio.http.Request;
-import one.nio.http.RequestMethod;
 import one.nio.http.Response;
 import one.nio.server.AcceptorConfig;
 import one.nio.util.Utf8;
@@ -22,17 +19,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class KeyValueService implements Service {
     private static final Logger log = LoggerFactory.getLogger(KeyValueService.class);
 
     private static final int FLUSH_THRESHOLD_BYTES = 4 * 1024 * 1024; // 4 mb
-    private static final String UNEXPECTED_ERROR = "Unexpected error";
-    private static final String ENDPOINT = "/v0/entity";
 
     private final ServiceConfig serviceConfig;
     private HttpServer server;
+    private HttpClient httpClient;
     private Dao<MemorySegment, Entry<MemorySegment>> dao;
 
     public KeyValueService(ServiceConfig serviceConfig) {
@@ -43,9 +44,9 @@ public class KeyValueService implements Service {
     public CompletableFuture<?> start() throws IOException {
         DaoConfig daoConfig = new DaoConfig(serviceConfig.workingDir(), FLUSH_THRESHOLD_BYTES);
         dao = new MemorySegmentDao(daoConfig);
+        httpClient = HttpClient.newHttpClient();
         HttpServerConfig httpServerConfig = createHttpServerConfigFromPort(serviceConfig.selfPort());
-        server = new KeyValueHttpServer(httpServerConfig);
-        server.addRequestHandlers(this);
+        server = new KeyValueHttpServer(httpServerConfig, this::handleRequest);
         server.start();
         return CompletableFuture.completedFuture(null);
     }
@@ -57,63 +58,48 @@ public class KeyValueService implements Service {
         return CompletableFuture.completedFuture(null);
     }
 
-    @Path(ENDPOINT)
-    @RequestMethod(Request.METHOD_GET)
-    public Response handleGet(@Param(value = "id", required = true) String id) {
+    private Response handleRequest(Request request, String id) {
         try {
-            if (id.isEmpty()) {
-                return badRequest();
+            List<String> clusterUrls = serviceConfig.clusterUrls();
+            String url = clusterUrls.get(getHashForKey(id) % clusterUrls.size());
+            if (!serviceConfig.selfUrl().equals(url)) {
+                return sendRequestToUrl(request, url);
             }
-            MemorySegment key = MemorySegment.ofArray(Utf8.toBytes(id));
-            Entry<MemorySegment> entry = dao.get(key);
-            if (entry == null) {
-                return new Response(Response.NOT_FOUND, Response.EMPTY);
-            }
-            return new Response(Response.OK, entry.value().toByteArray());
+
+            return switch (request.getMethod()) {
+                case Request.METHOD_GET -> handleGet(id);
+                case Request.METHOD_PUT -> handlePut(id, request.getBody());
+                case Request.METHOD_DELETE -> handleDelete(id);
+                default -> new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
+            };
         } catch (Exception e) {
-            log.error(UNEXPECTED_ERROR, e);
+            log.error("Unexpected error during processing {}", request, e);
             return internalError();
         }
     }
 
-    @Path(ENDPOINT)
-    @RequestMethod(Request.METHOD_PUT)
-    public Response handlePut(@Param(value = "id", required = true) String id, Request request) {
-        try {
-            if (id.isEmpty()) {
-                return badRequest();
-            }
-            MemorySegment key = MemorySegment.ofArray(Utf8.toBytes(id));
-            MemorySegment value = MemorySegment.ofArray(request.getBody());
-            Entry<MemorySegment> entry = new BaseEntry<>(key, value);
-            dao.upsert(entry);
-            return new Response(Response.CREATED, Response.EMPTY);
-        } catch (Exception e) {
-            log.error(UNEXPECTED_ERROR, e);
-            return internalError();
+    private Response handleGet(String id) throws IOException {
+        MemorySegment key = MemorySegment.ofArray(Utf8.toBytes(id));
+        Entry<MemorySegment> entry = dao.get(key);
+        if (entry == null) {
+            return new Response(Response.NOT_FOUND, Response.EMPTY);
         }
+        return new Response(Response.OK, entry.value().toByteArray());
     }
 
-    @Path(ENDPOINT)
-    @RequestMethod(Request.METHOD_DELETE)
-    public Response handleDelete(@Param(value = "id", required = true) String id) {
-        try {
-            if (id.isEmpty()) {
-                return badRequest();
-            }
-            MemorySegment key = MemorySegment.ofArray(Utf8.toBytes(id));
-            Entry<MemorySegment> entry = new BaseEntry<>(key, null);
-            dao.upsert(entry);
-            return new Response(Response.ACCEPTED, Response.EMPTY);
-        } catch (Exception e) {
-            log.error(UNEXPECTED_ERROR, e);
-            return internalError();
-        }
+    private Response handlePut(String id, byte[] body) {
+        MemorySegment key = MemorySegment.ofArray(Utf8.toBytes(id));
+        MemorySegment value = MemorySegment.ofArray(body);
+        Entry<MemorySegment> entry = new BaseEntry<>(key, value);
+        dao.upsert(entry);
+        return new Response(Response.CREATED, Response.EMPTY);
     }
 
-    @Path(ENDPOINT)
-    public Response handleNotAllowedMethod() {
-        return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
+    private Response handleDelete(String id) {
+        MemorySegment key = MemorySegment.ofArray(Utf8.toBytes(id));
+        Entry<MemorySegment> entry = new BaseEntry<>(key, null);
+        dao.upsert(entry);
+        return new Response(Response.ACCEPTED, Response.EMPTY);
     }
 
     private static HttpServerConfig createHttpServerConfigFromPort(int port) {
@@ -125,15 +111,24 @@ public class KeyValueService implements Service {
         return httpConfig;
     }
 
-    private static Response badRequest() {
-        return new Response(Response.BAD_REQUEST, Response.EMPTY);
+    private int getHashForKey(String id) {
+        return id.hashCode();
+    }
+
+    private Response sendRequestToUrl(Request request, String url) throws IOException, InterruptedException {
+        HttpRequest proxyRequest = HttpRequest.newBuilder(URI.create(url + request.getURI()))
+                .method(request.getMethodName(),
+                        HttpRequest.BodyPublishers.ofByteArray(request.getBody())
+                ).build();
+        HttpResponse<byte[]> response = httpClient.send(proxyRequest, HttpResponse.BodyHandlers.ofByteArray());
+        return new Response(String.valueOf(response.statusCode()), response.body());
     }
 
     private static Response internalError() {
         return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
     }
 
-    @ServiceFactory(stage = 2, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
+    @ServiceFactory(stage = 3, week = 2, bonuses = "SingleNodeTest#respectFileFolder")
     public static class StorageServiceFactory implements ServiceFactory.Factory {
 
         @Override
