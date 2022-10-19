57ed607864eb81c6e0e854891f61523f409c5e0a
Kirill Pobedonostsev <kirill.pobedonostsev@vk.team>
2022-10-20 01:44:36 +0500

    Up week

--- src/main/java/ok/dht/test/pobedonostsev/DemoService.java
+++ src/main/java/ok/dht/test/pobedonostsev/DemoService.java
@@ -29,7 +29,7 @@ public class DemoService implements Service {
         return CompletableFuture.completedFuture(null);
     }
 
-    @ServiceFactory(stage = 3, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
+    @ServiceFactory(stage = 3, week = 2, bonuses = "SingleNodeTest#respectFileFolder")
     public static class Factory implements ServiceFactory.Factory {
 
         @Override
