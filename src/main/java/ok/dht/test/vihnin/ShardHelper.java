a38d7c46ace44686ed045d94ef1026caa63166a7
Brat-vseznamus <fvixnin@gmail.com>
2022-10-19 19:00:03 +0300

    add solution + data

--- src/main/java/ok/dht/test/vihnin/ShardHelper.java
+++ src/main/java/ok/dht/test/vihnin/ShardHelper.java
@@ -2,15 +2,17 @@ package ok.dht.test.vihnin;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.NavigableSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class ShardHelper {
     private final Map<Integer, Integer> nodeToShard = new HashMap<>();
-    private final HashMap<Integer, Set<Integer>> shardToNodes = new HashMap<>();
-    private final TreeSet<Integer> nodeSet = new TreeSet<>();
+    private final Map<Integer, Set<Integer>> shardToNodes = new HashMap<>();
+    private final NavigableSet<Integer> nodeSet = new TreeSet<>();
 
     public Integer getShardByKey(String key) {
         Integer nextShard = nextOrEquals(key.hashCode());
@@ -20,13 +22,7 @@ public class ShardHelper {
     public Map<Integer, Set<HashRange>> addShard(Integer newShard, Set<Integer> vnodeHashes) {
         boolean init = nodeSet.isEmpty();
 
-        nodeSet.addAll(vnodeHashes);
-
-        for (var v: vnodeHashes) {
-            nodeToShard.put(v, newShard);
-        }
-
-        shardToNodes.put(newShard, vnodeHashes);
+        addNewVNodes(newShard, vnodeHashes);
 
         if (init) return new HashMap<>();
 
@@ -40,6 +36,27 @@ public class ShardHelper {
             visitedHashes.put(v, false);
         }
 
+        findRange(newShard, vnodeHashes, result, List.of(leftBorder, rightBorder), visitedHashes);
+
+        return result;
+    }
+
+    private void addNewVNodes(Integer newShard, Set<Integer> vnodeHashes) {
+        nodeSet.addAll(vnodeHashes);
+
+        for (var v: vnodeHashes) {
+            nodeToShard.put(v, newShard);
+        }
+
+        shardToNodes.put(newShard, vnodeHashes);
+    }
+
+    private void findRange(
+            Integer newShard,
+            Set<Integer> vnodeHashes,
+            Map<Integer, Set<HashRange>> result,
+            List<Integer> borders,
+            Map<Integer, Boolean> visitedHashes) {
         for (var hash: vnodeHashes) {
             if (visitedHashes.get(hash)) continue;
 
@@ -49,16 +66,20 @@ public class ShardHelper {
             while (nodeToShard.get(right).equals(newShard)) {
                 visitedHashes.put(right, true);
                 prevRight = right;
-                var nextRight = next(right);
-                right = nextRight != null ? nextRight : leftBorder;
+                right = next(right);
+                if (right == null) {
+                    right = borders.get(0);
+                }
             }
 
             var left = hash;
 
             while (nodeToShard.get(left).equals(newShard)) {
                 visitedHashes.put(left, true);
-                var prevLeft = prev(left);
-                left = prevLeft != null ? prevLeft : rightBorder;
+                left = prev(left);
+                if (left == null) {
+                    left = borders.get(1);
+                }
             }
 
             var rightShard = nodeToShard.get(right);
@@ -69,60 +90,6 @@ public class ShardHelper {
 
             result.get(rightShard).add(new HashRange(left + 1, prevRight));
         }
-
-        return result;
-    }
-
-    public Map<Integer, Set<HashRange>> removeShard(Integer shard) {
-        Set<Integer> vnodeHashes = shardToNodes.get(shard);
-        Map<Integer, Set<HashRange>> result = new HashMap<>();
-
-        Integer leftBorder = nodeSet.first();
-        Integer rightBorder = nodeSet.last();
-
-        var visitedHashes = new HashMap<Integer, Boolean>();
-
-        for (var v: vnodeHashes) {
-            visitedHashes.put(v, false);
-        }
-
-        for (var hash: vnodeHashes) {
-            if (visitedHashes.get(hash)) continue;
-
-            var right = hash;
-            var prevRight = right;
-
-            while (nodeToShard.get(right).equals(shard)) {
-                visitedHashes.put(right, true);
-                prevRight = right;
-                var nextRight = next(right);
-                right = nextRight != null ? nextRight : leftBorder;
-            }
-
-            var left = hash;
-
-            while (nodeToShard.get(left).equals(shard)) {
-                visitedHashes.put(left, true);
-                var prevLeft = prev(left);
-                left = prevLeft != null ? prevLeft : rightBorder;
-            }
-
-            var rightShard = nodeToShard.get(right);
-
-            if (!result.containsKey(rightShard)) {
-                result.put(rightShard, new HashSet<>());
-            }
-            result.get(rightShard).add(new HashRange(left + 1, prevRight));
-        }
-
-        shardToNodes.remove(shard);
-
-        for (var hash: vnodeHashes) {
-            nodeToShard.remove(hash);
-            nodeSet.remove(hash);
-        }
-
-        return result;
     }
 
     private Integer next(Integer hash) {
@@ -142,12 +109,6 @@ public class ShardHelper {
         if (res.equals(hash)) return nodeSet.floor(hash - 1);
         return res;
     }
-
-
-    private Integer prevOrEquals(Integer hash) {
-        return nodeSet.floor(hash);
-    }
-
 
     public static class HashRange {
         public final Integer leftBorder;
