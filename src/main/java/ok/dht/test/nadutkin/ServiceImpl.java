e06ab2eb2919e81da8949ce92e0c2b883d5ca30d
Федор Надуткин <FMNadutkin@sberbank.ru>
2022-10-25 01:20:19 +0300

    stage 2

--- src/main/java/ok/dht/test/nadutkin/ServiceImpl.java
+++ src/main/java/ok/dht/test/nadutkin/ServiceImpl.java
@@ -113,7 +113,7 @@ public class ServiceImpl implements Service {
         return httpConfig;
     }
 
-    @ServiceFactory(stage = 1, week = 1, bonuses = {"SingleNodeTest#respectFileFolder"})
+    @ServiceFactory(stage = 2, week = 2, bonuses = {"SingleNodeTest#respectFileFolder"})
     public static class Factory implements ServiceFactory.Factory {
 
         @Override
