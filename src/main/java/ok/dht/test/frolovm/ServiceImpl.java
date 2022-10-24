bbbbd69cefc971744510cb255866da2f1bf53622
mk17ru <mk17.ru@mail.ru>
2022-10-24 19:56:12 +0300

    fix week

--- src/main/java/ok/dht/test/frolovm/ServiceImpl.java
+++ src/main/java/ok/dht/test/frolovm/ServiceImpl.java
@@ -205,7 +205,7 @@ public class ServiceImpl implements Service {
         return CompletableFuture.completedFuture(null);
     }
 
-    @ServiceFactory(stage = 2, week = 2, bonuses = "SingleNodeTest#respectFileFolder")
+    @ServiceFactory(stage = 2, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
     public static class Factory implements ServiceFactory.Factory {
         @Override
         public Service create(ServiceConfig config) {
