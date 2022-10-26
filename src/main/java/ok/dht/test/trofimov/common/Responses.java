a52b2f70ba04f965ed875b2474b05d9ca499b053
Artyom Trofimov <troart@bk.ru>
2022-10-26 23:29:09 +0300

    fix codestyle

--- src/main/java/ok/dht/test/trofimov/common/Responses.java
+++ src/main/java/ok/dht/test/trofimov/common/Responses.java
@@ -1,10 +1,8 @@
 package ok.dht.test.trofimov.common;
 
-public class Responses {
+public final class Responses {
+    public static final String NOT_ENOUGH_REPLICAS = "504 Not Enough Replicas";
 
     private Responses() {
     }
-
-    public static final String NOT_ENOUGH_REPLICAS = "504 Not Enough Replicas";
-
 }
