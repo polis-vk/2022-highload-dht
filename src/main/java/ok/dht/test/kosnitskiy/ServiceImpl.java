e3c736efea2748f07c0970484dbe74117518d75b
Sanerins <askosnitskiy@gmail.com>
2022-10-26 14:43:47 +0300

    Change week because no need to make it higher

--- src/main/java/ok/dht/test/kosnitskiy/ServiceImpl.java
+++ src/main/java/ok/dht/test/kosnitskiy/ServiceImpl.java
@@ -52,7 +52,7 @@ public class ServiceImpl implements Service {
         return CompletableFuture.completedFuture(null);
     }
 
-    @ServiceFactory(stage = 3, week = 2, bonuses = "SingleNodeTest#respectFileFolder")
+    @ServiceFactory(stage = 3, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
     public static class Factory implements ServiceFactory.Factory {
 
         @Override
