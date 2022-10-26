936f92c613ce8634721fac9ea8c14857c6f3874c
Mizzza54 <gerasimov.misha2001@gmail.com>
2022-10-26 22:15:52 +0300

    STAGE-4: коваленко убил

--- src/main/java/ok/dht/test/kovalenko/scripts/get/get-existing-fixed.lua
+++
@@ -1,4 +0,0 @@
-raw_path = "/v0/entity?id=k150000000"
-request = function()
-    return wrk.format("GET", raw_path)
-end
--- src/main/java/ok/dht/test/kovalenko/scripts/get/get-existing-random.lua
+++
@@ -1,6 +0,0 @@
-raw_path = "/v0/entity?id=k"
-request = function()
-    local r = math.random(1, 300000000)
-    path = raw_path .. r
-    return wrk.format("GET", path)
-end
