aa5c7769500b9e688cc842ac78a44a51eae5b1d5
Artyom Trofimov <troart@bk.ru>
2022-10-26 23:19:25 +0300

    clean code

--- src/main/java/ok/dht/test/trofimov/dao/MyHttpServer.java
+++ src/main/java/ok/dht/test/trofimov/dao/MyHttpServer.java
@@ -319,29 +319,7 @@ public class MyHttpServer extends HttpServer {
                 .build();
 
         HttpResponse<byte[]> response = client.send(proxyRequest, HttpResponse.BodyHandlers.ofByteArray());
-        String status = switch (response.statusCode()) {
-            case HttpURLConnection.HTTP_OK -> Response.OK;
-            case HttpURLConnection.HTTP_CREATED -> Response.CREATED;
-            case HttpURLConnection.HTTP_ACCEPTED -> Response.ACCEPTED;
-            case HttpURLConnection.HTTP_NO_CONTENT -> Response.NO_CONTENT;
-            case HttpURLConnection.HTTP_SEE_OTHER -> Response.SEE_OTHER;
-            case HttpURLConnection.HTTP_NOT_MODIFIED -> Response.NOT_MODIFIED;
-            case HttpURLConnection.HTTP_USE_PROXY -> Response.USE_PROXY;
-            case HttpURLConnection.HTTP_BAD_REQUEST -> Response.BAD_REQUEST;
-            case HttpURLConnection.HTTP_UNAUTHORIZED -> Response.UNAUTHORIZED;
-            case HttpURLConnection.HTTP_PAYMENT_REQUIRED -> Response.PAYMENT_REQUIRED;
-            case HttpURLConnection.HTTP_FORBIDDEN -> Response.FORBIDDEN;
-            case HttpURLConnection.HTTP_NOT_FOUND -> Response.NOT_FOUND;
-            case HttpURLConnection.HTTP_NOT_ACCEPTABLE -> Response.NOT_ACCEPTABLE;
-            case HttpURLConnection.HTTP_CONFLICT -> Response.CONFLICT;
-            case HttpURLConnection.HTTP_GONE -> Response.GONE;
-            case HttpURLConnection.HTTP_LENGTH_REQUIRED -> Response.LENGTH_REQUIRED;
-            case HttpURLConnection.HTTP_INTERNAL_ERROR -> Response.INTERNAL_ERROR;
-            case HttpURLConnection.HTTP_NOT_IMPLEMENTED -> Response.NOT_IMPLEMENTED;
-            case HttpURLConnection.HTTP_BAD_GATEWAY -> Response.BAD_GATEWAY;
-            case HttpURLConnection.HTTP_GATEWAY_TIMEOUT -> Response.GATEWAY_TIMEOUT;
-            default -> throw new IllegalArgumentException("Unknown status code: " + response.statusCode());
-        };
+        String status = getResponseStatusCode(response.statusCode());
         Response result = new Response(status, response.body());
         Optional<String> headerTimestamp = response.headers().firstValue("x-timestamp");
         Optional<String> headerTomb = response.headers().firstValue("x-tomb");
