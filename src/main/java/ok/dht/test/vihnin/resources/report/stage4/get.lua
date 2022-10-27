272769537bdce2379da8567b5b157819611830d2
Brat-vseznamus <fvixnin@gmail.com>
2022-10-27 18:40:12 +0300

    отчет 3

--- src/main/java/ok/dht/test/vihnin/resources/report/stage4/get.lua
+++ src/main/java/ok/dht/test/vihnin/resources/report/stage4/get.lua
@@ -4,5 +4,5 @@ cnt = 1
 
 function request()
     cnt = cnt + 1
-    return wrk.format("GET", "/v0/entity?id=" .. math.random(1000000, 4000000), {}, nil)
+    return wrk.format("GET", "/v0/entity?id=" .. cnt, {}, nil)
 end
