2a74ad2787b517d4258820ecb4fa22f511f1d56d
ayasevich <artem.yasevich@mail.ru>
2022-10-29 18:55:41 +0300

    Refactor ReplicasManager class

--- src/main/java/ok/dht/test/yasevich/ReplicasManager.java
+++ src/main/java/ok/dht/test/yasevich/ReplicasManager.java
@@ -6,15 +6,15 @@ import one.nio.http.Request;
 import one.nio.http.Response;
 
 import java.io.IOException;
+import java.net.http.HttpResponse;
 import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
 
 public class ReplicasManager {
+
     public static final String NOT_ENOUGH_REPLICAS = "504 Not Enough Replicas";
 
     private final TimeStampingDao dao;
@@ -34,6 +34,7 @@ public class ReplicasManager {
         this.executor = executor;
     }
 
+
     void handleReplicatingRequest(HttpSession session, Request request, String key, int ack, int from) {
         Queue<RandevouzHashingRouter.Node> responsibleNodes = shardingRouter.responsibleNodes(key, from);
         if (responsibleNodes.isEmpty()) {
@@ -41,9 +42,9 @@ public class ReplicasManager {
         }
         switch (request.getMethod()) {
             case Request.METHOD_PUT ->
-                    handleUpsert(session, request, key, responsibleNodes, ack, from, Response.CREATED);
+                    handleUpsert(session, request, key, responsibleNodes, ack, from, Response.CREATED, 201);
             case Request.METHOD_DELETE ->
-                    handleUpsert(session, request, key, responsibleNodes, ack, from, Response.ACCEPTED);
+                    handleUpsert(session, request, key, responsibleNodes, ack, from, Response.ACCEPTED, 202);
             case Request.METHOD_GET -> handleGet(session, request, key, responsibleNodes, ack, from);
             default -> sendResponse(session, new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY));
         }
@@ -54,178 +55,24 @@ public class ReplicasManager {
             String key,
             Iterable<RandevouzHashingRouter.Node> responsibleNodes,
             int ack, int from,
-            String okMessage
+            String okMessage, int okStatusCode
     ) {
-        AtomicInteger oks = new AtomicInteger();
-        AtomicInteger responsesTotal = new AtomicInteger();
-        AtomicBoolean alreadyResponded = new AtomicBoolean();
+        ReplicatingResponseCounter counter = new ReplicatingResponseCounter(ack, from);
 
-        for (RandevouzHashingRouter.Node node : responsibleNodes) {
-            if (node.url.equals(selfUrl)) {
-                try {
-                    doUpsert(request, key);
-                    probablyResponseGood(session, goodResponseForUpsert(okMessage), ack, oks,
-                            responsesTotal, alreadyResponded);
-                } catch (Exception e) {
-                    handleNodeFailure(e, node, request, key);
-                    probablyResponseBad(session, from, responsesTotal, alreadyResponded);
-                }
-                continue;
-            }
-            shardingRouter.routedRequestFuture(request, key, node)
-                    .orTimeout(ServiceImpl.ROUTED_REQUEST_TIMEOUT_MS, TimeUnit.MILLISECONDS)
-                    .whenCompleteAsync((httpResponse, throwable) -> {
-                        if (throwable != null) {
-                            probablyResponseBad(session, from, responsesTotal, alreadyResponded);
-                            handleNodeFailure(throwable, node, request, key);
-                            return;
-                        }
-                        if (httpResponse.statusCode() == Integer.parseInt(okMessage.substring(0, 3))) {
-                            probablyResponseGood(session, goodResponseForUpsert(okMessage), ack,
-                                    oks, responsesTotal, alreadyResponded);
-                        } else {
-                            probablyResponseBad(session, from, responsesTotal, alreadyResponded);
-                        }
-                    }, executor);
-        }
-    }
-
-    private void handleGet(
-            HttpSession session, Request request,
-            String key,
-            Iterable<RandevouzHashingRouter.Node> responsibleNodes,
-            int ack, int from
-    ) {
-        AtomicInteger nonFailed = new AtomicInteger();
-        AtomicInteger responsesTotal = new AtomicInteger();
-        AtomicBoolean alreadyResponded = new AtomicBoolean();
-        TimeStampedValue[] latestValueRef = new TimeStampedValue[1];
-        Lock lock = new ReentrantLock();
-        for (RandevouzHashingRouter.Node node : responsibleNodes) {
-            if (node.url.equals(selfUrl)) {
-                try {
-                    TimeStampedValue value = dao.get(key);
-                    value = updateValueIfNeeded(latestValueRef, lock, value);
-                    probablyResponseGoodToGet(session, ack, value, nonFailed, responsesTotal, alreadyResponded);
-                } catch (Exception e) {
-                    handleNodeFailure(e, node, request, key);
-                    probablyResponseBad(session, from, responsesTotal, alreadyResponded);
-                }
-                continue;
-            }
-            shardingRouter.routedRequestFuture(request, key, node)
-                    .orTimeout(ServiceImpl.ROUTED_REQUEST_TIMEOUT_MS, TimeUnit.MILLISECONDS)
-                    .whenCompleteAsync((httpResponse, throwable) -> {
-                        if (throwable != null) {
-                            handleNodeFailure(throwable, node, request, key);
-                            probablyResponseBad(session, from, responsesTotal, alreadyResponded);
-                            return;
-                        }
-                        if (httpResponse.statusCode() != 200 && httpResponse.statusCode() != 404) {
-                            probablyResponseBad(session, from, responsesTotal, alreadyResponded);
-                            return;
-                        }
-                        TimeStampedValue value = updateValueIfNeeded(latestValueRef, lock, httpResponse.body(),
-                                httpResponse.statusCode());
-                        probablyResponseGoodToGet(session, ack, value, nonFailed, responsesTotal, alreadyResponded);
-                    }, executor);
-        }
-    }
-
-    private TimeStampedValue updateValueIfNeeded(
-            TimeStampedValue[] latestValueRef,
-            Lock lock,
-            byte[] body,
-            int statusCode
-    ) {
-        lock.lock();
-        try {
-            TimeStampedValue value;
-            if (statusCode == 404) {
-                if (body.length == 0) {
-                    return latestValueRef[0];
-                }
-                value = TimeStampedValue.tombstoneFromTime(body);
+        Consumer<HttpResponse<byte[]>> consumer = (httpResponse) -> {
+            if (httpResponse.statusCode() == okStatusCode) {
+                responseToUpsertIfNeeded(session, okMessage, counter);
             } else {
-                value = TimeStampedValue.fromBytes(body);
+                counter.responseFailureIfNeeded(session);
             }
-            if (latestValueRef[0] == null || value.time > latestValueRef[0].time) {
-                latestValueRef[0] = value;
-            }
-            return latestValueRef[0];
-        } finally {
-            lock.unlock();
-        }
-    }
+        };
 
-    private TimeStampedValue updateValueIfNeeded(TimeStampedValue[] latestValueRef, Lock lock, TimeStampedValue value) {
-        lock.lock();
-        try {
-            if (latestValueRef[0] == null || value.time > latestValueRef[0].time) {
-                latestValueRef[0] = value;
-            }
-            return latestValueRef[0];
-        } finally {
-            lock.unlock();
-        }
-    }
+        Runnable daoWork = () -> {
+            doUpsert(request, key);
+            responseToUpsertIfNeeded(session, okMessage, counter);
+        };
 
-    private static void probablyResponseGood(
-            HttpSession session,
-            Response goodResponse,
-            int ack,
-            AtomicInteger okResponses,
-            AtomicInteger totalResponses,
-            AtomicBoolean alreadyResponded
-    ) {
-        if (okResponses.incrementAndGet() == ack) {
-            sendResponse(session, goodResponse);
-            alreadyResponded.set(true);
-        }
-        totalResponses.incrementAndGet();
-    }
-
-    private static void probablyResponseGoodToGet(
-            HttpSession session,
-            int ack,
-            TimeStampedValue latestValue,
-            AtomicInteger nonFailedResponses,
-            AtomicInteger totalResponses,
-            AtomicBoolean alreadyResponded
-    ) {
-        totalResponses.incrementAndGet();
-        if (nonFailedResponses.incrementAndGet() != ack) {
-            return;
-        }
-        if (latestValue == null || latestValue.value == null) {
-            sendResponse(session, new Response(Response.NOT_FOUND, Response.EMPTY));
-        } else {
-            sendResponse(session, new Response(Response.OK, latestValue.valueBytes()));
-        }
-        alreadyResponded.set(true);
-    }
-
-    private static void probablyResponseBad(
-            HttpSession session,
-            int from,
-            AtomicInteger totalResponses,
-            AtomicBoolean alreadyResponded
-    ) {
-        if (totalResponses.incrementAndGet() == from) {
-            if (!alreadyResponded.get()) {
-                sendResponse(session, new Response(NOT_ENOUGH_REPLICAS, Response.EMPTY));
-            }
-        }
-    }
-
-    private static Response goodResponseForUpsert(String okMessage) {
-        return new Response(okMessage, Response.EMPTY);
-    }
-
-    private static void handleNodeFailure(Throwable t, RandevouzHashingRouter.Node node, Request request, String key) {
-        node.managePossibleIllness();
-        ServiceImpl.LOGGER.error(t + " when requesting a " + node.url + " to respond to an "
-                + request.getMethod() + " method for " + key);
+        handle(session, request, key, responsibleNodes, counter, consumer, daoWork);
     }
 
     private void doUpsert(Request request, String key) {
@@ -236,12 +83,105 @@ public class ReplicasManager {
         }
     }
 
+    private void handleGet(
+            HttpSession session, Request request,
+            String key,
+            Iterable<RandevouzHashingRouter.Node> responsibleNodes,
+            int ack, int from
+    ) {
+        ReplicatingGetAggregator aggregator = new ReplicatingGetAggregator(ack, from);
+
+        Consumer<HttpResponse<byte[]>> consumer = (httpResponse) -> {
+            TimeStampedValue value = aggregator.updateValueIfNeeded(valueFromResponse(httpResponse));
+            responseToGetIfNeeded(session, aggregator, value);
+        };
+
+        Runnable daoOperator = () -> {
+            TimeStampedValue value = aggregator.updateValueIfNeeded(dao.get(key));
+            responseToGetIfNeeded(session, aggregator, value);
+        };
+
+        handle(session, request, key, responsibleNodes, aggregator, consumer, daoOperator);
+    }
+
+    private void handle(
+            HttpSession session, Request request,
+            String key,
+            Iterable<RandevouzHashingRouter.Node> responsibleNodes,
+            ReplicatingResponseCounter counter,
+            Consumer<HttpResponse<byte[]>> httpResponseConsumer,
+            Runnable daoWork
+    ) {
+        for (RandevouzHashingRouter.Node node : responsibleNodes) {
+            if (selfUrl.equals(node.url)) {
+                CompletableFuture.runAsync(daoWork).exceptionally(throwable -> {
+                    counter.responseFailureIfNeeded(session);
+                    handleFailure(throwable, selfUrl, request, key);
+                    return null;
+                });
+                continue;
+            }
+            shardingRouter.routedRequestFuture(request, key, node)
+                    .orTimeout(ServiceImpl.ROUTED_REQUEST_TIMEOUT_MS, TimeUnit.MILLISECONDS)
+                    .whenComplete((httpResponse, throwable) -> {
+                        if (throwable == null) {
+                            httpResponseConsumer.accept(httpResponse);
+                            return;
+                        }
+                        counter.responseFailureIfNeeded(session);
+                        handleNodeFailure(throwable, node, request, key);
+                    });
+        }
+    }
+
     static void sendResponse(HttpSession session, Response response) {
         try {
             session.sendResponse(response);
         } catch (IOException e) {
             ServiceImpl.LOGGER.error("Error when sending " + response.getStatus());
             session.close();
+        }
+    }
+
+    private static void responseToUpsertIfNeeded(HttpSession session, String okMessage, ReplicatingResponseCounter counter) {
+        if (counter.isTimeToResponseGood()) {
+            sendResponse(session, new Response(okMessage, Response.EMPTY));
+        }
+    }
+
+    private static void responseToGetIfNeeded(HttpSession session, ReplicatingGetAggregator counter, TimeStampedValue value) {
+        if (!counter.isTimeToResponseGood()) {
+            return;
+        }
+        if (value == null || value.value == null) {
+            sendResponse(session, new Response(Response.NOT_FOUND, Response.EMPTY));
+        } else {
+            sendResponse(session, new Response(Response.OK, value.valueBytes()));
+        }
+    }
+
+    private static void handleFailure(Throwable t, String selfUrl, Request request, String key) {
+        ServiceImpl.LOGGER.error(t + " when " + selfUrl + " was proceeding a request method "
+                + request.getMethod() + " for " + key);
+    }
+
+    private static void handleNodeFailure(Throwable t, RandevouzHashingRouter.Node node, Request request, String key) {
+        node.managePossibleIllness();
+        ServiceImpl.LOGGER.error(t + " when requesting a " + node.url + " to respond to an "
+                + request.getMethod() + " method for " + key);
+    }
+
+    private static TimeStampedValue valueFromResponse(HttpResponse<byte[]> httpResponse) {
+        if (httpResponse.statusCode() != 200 && httpResponse.statusCode() != 404) {
+            return null;
+        }
+        if (httpResponse.statusCode() == 404) {
+            if (httpResponse.body().length == 0) {
+                return null;
+            }
+            return TimeStampedValue.tombstoneFromTime(httpResponse.body());
+        } else {
+            return TimeStampedValue.fromBytes(httpResponse.body());
         }
     }
 
