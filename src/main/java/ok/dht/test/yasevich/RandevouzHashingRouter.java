cc3f8977e6ce6e68859e90fc653b4e28193fbf6e
ayasevich <artem.yasevich@mail.ru>
2022-11-02 22:25:50 +0300

    Make nodes' failures count in a window

--- src/main/java/ok/dht/test/yasevich/RandevouzHashingRouter.java
+++ src/main/java/ok/dht/test/yasevich/RandevouzHashingRouter.java
@@ -8,26 +8,28 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.ToIntFunction;
 
 public class RandevouzHashingRouter {
-    public static final int ILL_NODE_SKIPPED_REQUESTS = 250;
     public static final int FAILED_REQUESTS_THRESHOLD = 50;
+    public static final int FAILED_REQUESTS_WINDOW_MS = 100;
+    public static final int ILLNESS_PERIOD_MS = 100;
 
     private final List<Node> nodes;
     private final HttpClient httpClient;
 
     public RandevouzHashingRouter(List<String> clusterUrls, Executor executor) {
-        this.nodes = new ArrayList<>(clusterUrls.size());
         this.httpClient = HttpClient.newBuilder().executor(executor).build();
+        this.nodes = new ArrayList<>(clusterUrls.size());
         for (String url : clusterUrls) {
             nodes.add(new Node(url));
         }
@@ -70,17 +72,18 @@ public class RandevouzHashingRouter {
 
     public class Node {
         final String url;
-        private final AtomicInteger counter = new AtomicInteger();
-        private boolean isIll;
+        private final Deque<Long> failedRequestsTimings = new ArrayDeque<>(FAILED_REQUESTS_THRESHOLD);
+        private volatile long illnessStartTime = -1;
 
         private Node(String url) {
             this.url = url;
         }
 
         public CompletableFuture<HttpResponse<byte[]>> routedRequestFuture(Request request, String key, long time) {
-            if (isIll) {
-                managePossibleRecovery();
-                if (isIll) {
+            if (illnessStartTime > 0) {
+                if (mayHaveRecovered()) {
+                    illnessStartTime = -1;
+                } else {
                     return CompletableFuture.failedFuture(new ConnectException("Node is ill"));
                 }
             }
@@ -88,24 +91,28 @@ public class RandevouzHashingRouter {
             return httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofByteArray());
         }
 
-        private void managePossibleRecovery() {
-            if (counter.incrementAndGet() >= ILL_NODE_SKIPPED_REQUESTS) {
-                isIll = false;
-                counter.set(0);
-            }
+        private boolean mayHaveRecovered() {
+            long illnessStart = illnessStartTime;
+            return illnessStart < 0 || System.currentTimeMillis() - illnessStart > ILLNESS_PERIOD_MS;
         }
 
-        void managePossibleIllness() {
-            if (counter.incrementAndGet() >= FAILED_REQUESTS_THRESHOLD) {
-                isIll = true;
-                counter.set(0);
+        synchronized void managePossibleIllness() {
+            long time = System.currentTimeMillis();
+            if (failedRequestsTimings.size() < FAILED_REQUESTS_THRESHOLD) {
+                failedRequestsTimings.add(time);
+                return;
             }
+            if (time - failedRequestsTimings.peek() < FAILED_REQUESTS_WINDOW_MS) {
+                illnessStartTime = time;
+                return;
+            }
+            failedRequestsTimings.pop();
+            failedRequestsTimings.add(time);
         }
 
         @Override
         public int hashCode() {
             return url.hashCode();
         }
-
     }
 }
