ec06d4bedd0370a9eb1f0619b3fb7a5af719a444
Veronika <58723287+interestIngc@users.noreply.github.com>
2022-11-02 18:28:40 +0100

    fixed stage

--- src/main/java/ok/dht/test/anikina/ServiceImpl.java
+++ src/main/java/ok/dht/test/anikina/ServiceImpl.java
@@ -30,7 +30,7 @@ public class ServiceImpl implements Service {
         return CompletableFuture.completedFuture(null);
     }
 
-    @ServiceFactory(stage = 4, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
+    @ServiceFactory(stage = 5, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
     public static class Factory implements ServiceFactory.Factory {
         @Override
         public Service create(ServiceConfig config) {
