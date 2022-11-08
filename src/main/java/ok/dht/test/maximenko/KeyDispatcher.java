fce1ce4101e2ccb0d79cf5b9e78636bf02e06010
Dmitree-Max <d.s.maximenko@gmail.com>
2022-11-09 00:27:49 +0300

    Implemented fair key distribution

--- src/main/java/ok/dht/test/maximenko/KeyDispatcher.java
+++ src/main/java/ok/dht/test/maximenko/KeyDispatcher.java
@@ -1,12 +1,28 @@
 package ok.dht.test.maximenko;
 
+import java.util.List;
+import java.util.TreeMap;
+
 public class KeyDispatcher {
-    private final int nodeAmount;
-    KeyDispatcher(int nodeAmount) {
-        this.nodeAmount = nodeAmount;
+
+    private final static int virtualNodesPerMachine = 5;
+    TreeMap<Integer, Integer> virtualNodes;
+    KeyDispatcher(List<Integer> nodes) {
+        int nodeAmount = nodes.size();
+        int interval = Integer.MAX_VALUE / (nodeAmount * virtualNodesPerMachine);
+        virtualNodes = new TreeMap<>();
+        int border = interval;
+        for (int i = 0; i < virtualNodesPerMachine; i++) {
+            for (Integer node : nodes) {
+                virtualNodes.put(border, node);
+                border += interval;
+            }
+        }
+
     }
 
     int getNode(String key) {
-        return 0;
+        int hash = key.hashCode();
+        return virtualNodes.higherEntry(hash).getValue();
     }
 }
