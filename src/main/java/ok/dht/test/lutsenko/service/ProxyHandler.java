cb81159a33b8405bffc4a1e94054507a2216f0e4
LutsenkoDm <lutsenkodm@mail.ru>
2022-11-09 16:54:25 +0300

    Fix Using the '.*' form of import

--- src/main/java/ok/dht/test/lutsenko/service/ProxyHandler.java
+++ src/main/java/ok/dht/test/lutsenko/service/ProxyHandler.java
@@ -14,7 +14,14 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinWorkerThread;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ProxyHandler implements Closeable {
