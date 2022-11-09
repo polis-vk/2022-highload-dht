64fd4e35cd2f786e7e4ce68c85d21ee367f27929
Dmitree-Max <d.s.maximenko@gmail.com>
2022-11-09 15:11:08 +0300

    Implemented cluster start for testing

--- src/main/java/ok/dht/test/maximenko/Server.java
+++ src/main/java/ok/dht/test/maximenko/Server.java
@@ -1,9 +1,8 @@
 package ok.dht.test.maximenko;
 
-
 import ok.dht.ServiceConfig;
 import java.nio.file.Files;
-import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Server {
@@ -11,15 +10,42 @@ public class Server {
 
     }
     public static void main(String[] args) throws Exception {
-        int port = 19234;
-        String url = "http://localhost:" + port;
-        ServiceConfig cfg = new ServiceConfig(
-                port,
-                url,
-                Collections.singletonList(url),
-                Files.createTempDirectory("server")
+        int firstPort = 19234;
+        int secondPort = 19235;
+        int thirdPort = 19236;
+
+        String url = "http://localhost:";
+        String firstUrl  = url + firstPort;
+        String secondUrl = url + secondPort;
+        String thirdUrl  = url + thirdPort;
+
+        List<String> clusterUrls = List.of(firstUrl, secondUrl, thirdUrl);
+
+        ServiceConfig cfg1 = new ServiceConfig(
+                firstPort,
+                firstUrl,
+                clusterUrls,
+                Files.createTempDirectory("server1")
         );
-        new DatabaseService(cfg).start().get(1, TimeUnit.SECONDS);
-        System.out.println("Server is listening on port: " + port);
+
+        ServiceConfig cfg2 = new ServiceConfig(
+                secondPort,
+                secondUrl,
+                clusterUrls,
+                Files.createTempDirectory("server2")
+        );
+
+        ServiceConfig cfg3 = new ServiceConfig(
+                thirdPort,
+                thirdUrl,
+                clusterUrls,
+                Files.createTempDirectory("server3")
+        );
+        new DatabaseService(cfg1).start().get(1, TimeUnit.SECONDS);
+        System.out.println("Server is listening on port: " + firstPort);
+        new DatabaseService(cfg2).start().get(1, TimeUnit.SECONDS);
+        System.out.println("Server is listening on port: " + secondPort);
+        new DatabaseService(cfg3).start().get(1, TimeUnit.SECONDS);
+        System.out.println("Server is listening on port: " + thirdPort);
     }
 }
