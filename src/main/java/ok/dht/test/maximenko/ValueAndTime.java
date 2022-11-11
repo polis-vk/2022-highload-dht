ac01b66335479edb77c9e464c4873795b519cb2e
Dmitree-Max <d.s.maximenko@gmail.com>
2022-11-11 03:32:15 +0300

    Codestyle. Refactoring. 5.

--- src/main/java/ok/dht/test/maximenko/ValueAndTime.java
+++ src/main/java/ok/dht/test/maximenko/ValueAndTime.java
@@ -7,10 +7,10 @@ class ValueAndTime {
     final long time;
 
     public ValueAndTime(byte[] value, long time) {
-        if (value != null) {
-            this.value = Arrays.copyOf(value, value.length);
-        } else {
+        if (value == null) {
             this.value = null;
+        } else {
+            this.value = Arrays.copyOf(value, value.length);
         }
         this.time = time;
     }
