8653b6592470bca2cef93b3b9387a2d407d03371
Veronika <58723287+interestIngc@users.noreply.github.com>
2022-11-16 15:20:35 +0300

    updates

--- src/main/java/ok/dht/test/anikina/lua/put.lua
+++ src/main/java/ok/dht/test/anikina/lua/put.lua
@@ -1,7 +1,7 @@
 counter = 0
 
 request = function()
-    path = "/v0/entity?id=keyNumber" .. counter
+    path = "/v0/entity?id=" .. counter
     body = string.rep("string", 60)
     counter = counter + 1
     return "PUT " .. path .. " HTTP/1.1\r\nContent-Length: 360\r\n\r\n" .. body
