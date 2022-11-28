1e898959d211919a5c11720c0a6f65bdc98d6c3a
AlinaShestakova <shestakova.ayu@edu.spbstu.ru>
2022-11-28 19:53:33 +0300

    Revert "Resolving conflicts"
    This reverts commit 0cc40011

--- src/main/java/ok/dht/test/pashchenko/DemoService.java
+++ src/main/java/ok/dht/test/pashchenko/DemoService.java
@@ -32,7 +32,7 @@ public class DemoService implements Service {
     }
 
 
-    @ServiceFactory(stage = 6, week = 2)
+    @ServiceFactory(stage = 3, week = 1, bonuses = "SingleNodeTest#respectFileFolder")
     public static class Factory implements ServiceFactory.Factory {
 
         @Override
